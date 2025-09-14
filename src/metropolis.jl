a8a85c386c38fd38ad226711942334c976974155
PatXue <95881915+PatXue@users.noreply.github.com>
2025-09-14 12:01:29 -0400

    Update metropolis sweeps to change eta

--- src/metropolis.jl
+++ src/metropolis.jl
@@ -1,18 +1,31 @@
-function Carlo.sweep!(mc::MC{:Metropolis}, rng::AbstractRNG=default_rng())
+function Carlo.sweep!(mc::WignerMC{:Metropolis}, rng::AbstractRNG=default_rng())
     Lx, Ly = size(mc.spins)
     for _ in 1:length(mc.spins)
-        # Select site for spin change
-        x = rand(rng, 1:Lx)
-        y = rand(rng, 1:Ly)
-        old_s = mc.spins[x, y]
-        # Propose new spin vector
-        new_s = rand(rng, SpinVector)
-        ΔE = energy(mc, new_s, x, y) - energy(mc, old_s, x, y)
+        for type in (:s, :η)
+            # Select site for spin change
+            x = rand(rng, 1:Lx)
+            y = rand(rng, 1:Ly)
 
-        # Probability of accepting spin flip (for ΔE ≤ 0 always accept)
-        prob = exp(-ΔE / mc.T)
-        if prob >= 1.0 || rand(rng) < prob
-            mc.spins[x, y] = new_s
+            old_s = mc.spins[x, y]
+            old_η = mc.ηs[x, y]
+            old_E = energy(mc, old_s, old_η, x, y)
+            new_s = new_η = rand(rng, SpinVector)
+            if type == :s
+                new_E = energy(mc, new_s, old_η, x, y)
+            elseif type == :η
+                new_E = energy(mc, old_s, new_η, x, y)
+            end
+            ΔE = new_E - old_E
+
+            # Probability of accepting spin flip (for ΔE ≤ 0 always accept)
+            prob = exp(-ΔE / mc.T)
+            if prob >= 1.0 || rand(rng) < prob
+                if type == :s
+                    mc.spins = new_s
+                elseif type == :η
+                    mc.ηs = new_η
+                end
+            end
         end
     end
     return nothing
