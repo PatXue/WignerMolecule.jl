d5003804f1b29db45ca7057bc09b64a4e6a88165
PatXue <95881915+PatXue@users.noreply.github.com>
2025-09-14 11:36:53 -0400

    Remove nearest neighbor functions from utils

--- src/utils.jl
+++ src/utils.jl
@@ -4,10 +4,3 @@ function Random.rand(rng::AbstractRNG, ::Random.SamplerType{SpinVector})
     θ = acos(2 * rand(rng) - 1)
     return SpinVector(cos(ϕ)sin(θ), sin(ϕ)sin(θ), cos(θ))
 end
-
-# Sum spins of position (x, y)'s nearest neighbors
-nn_sum(A::AbstractArray, x, y) = A[x+1, y] + A[x, y+1] + A[x-1, y] + A[x, y-1]
-# Sum spins of (x, y)'s next nearest NE-SW neighbors
-nnna_sum(A::AbstractArray, x, y) = A[x+1, y+1] + A[x-1, y-1]
-# Sum spins of (x, y)'s next nearest NW-SE neighbors
-nnnb_sum(A::AbstractArray, x, y) = A[x+1, y-1] + A[x-1, y+1]
