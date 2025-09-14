6c6f913f29ffc953370ef033c56b53601fc7b89b
PatXue <95881915+PatXue@users.noreply.github.com>
2025-09-14 11:06:26 -0400

    Move energy functions to mc.jl
    Holds for any type of algorithm

--- src/metropolis.jl
+++ src/metropolis.jl
@@ -1,13 +1,3 @@
-# Calculate the energy at a lattice site (x, y) if it had spin s
-function energy(mc::MC{:Metropolis}, s::SpinVector, x, y)
-    nn = nn_sum(mc.spins, x, y)
-    nnna = nnna_sum(mc.spins, x, y)
-    nnnb = nnnb_sum(mc.spins, x, y)
-    H0 = s ⋅ (mc.J1 * nn + mc.J2a * nnna + mc.J2b * nnnb)
-    biquad = mc.K * ((s ⋅ mc.spins[x-1, y])^2 + (s ⋅ mc.spins[x+1, y])^2)
-    return H0 + biquad
-end
-
 function Carlo.sweep!(mc::MC{:Metropolis}, rng::AbstractRNG=default_rng())
     Lx, Ly = size(mc.spins)
     for _ in 1:length(mc.spins)
@@ -26,16 +16,4 @@ function Carlo.sweep!(mc::MC{:Metropolis}, rng::AbstractRNG=default_rng())
         end
     end
     return nothing
-end
-
-# Calculate the energy contribution of a site (x, y), considering only half of
-# its bonds (avoids double counting when calculating total energy)
-function half_energy(mc::MC{:Metropolis}, x, y)
-    s = mc.spins[x, y]
-    nn = mc.spins[x+1, y] + mc.spins[x, y+1]
-    nnna = mc.spins[x+1, y+1]
-    nnnb = mc.spins[x+1, y-1]
-    H0 = s ⋅ (mc.J1 * nn + mc.J2a * nnna + mc.J2b * nnnb)
-    biquad = mc.K * (s ⋅ mc.spins[x+1, y])^2
-    return H0 + biquad
 end
