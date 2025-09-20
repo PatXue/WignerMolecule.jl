8d19a92ac171379db0429b5377a853d2f2f19085
PatXue <95881915+PatXue@users.noreply.github.com>
2025-09-20 19:16:09 -0400

    Finer temperature sweep

--- jobs/small-sys.jl
+++ jobs/small-sys.jl
@@ -9,14 +9,14 @@ tm = TaskMaker()
 
 L = 20
 tm.Lx = tm.Ly = L
-tm.sweeps = 50000
+tm.sweeps = 20000
 tm.thermalization = 0
 tm.binsize = 100
 
 tm.savefreq = 5000
 
-tm.wigparams = WignerParams(-2.12742, -7.37151, -2.60026, (-1.5492-3.67457im), (2.22406+3.06319im), 0.672249, (0.215811+0.332566im), 0.815589, 1e-3)
-Ts = 1:10
+tm.wigparams = WignerParams(-2.12742, -7.37151, -2.60026, (-1.5492-3.67457im), (2.22406+3.06319im), 0.672249, (0.215811+0.332566im), 0.815589, 1e-2)
+Ts = 1:0.25:5
 for T in Ts
     tm.T = T
     spins_dir = "small-sys.data/$(current_task_name(tm))"
