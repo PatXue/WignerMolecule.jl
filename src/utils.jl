f05f4b6a9234ad5956b72e037d316d936c662b16
PatXue <95881915+PatXue@users.noreply.github.com>
2025-09-27 17:44:39 -0400

    Refactor initialization functions

--- src/utils.jl
+++ src/utils.jl
@@ -1,29 +1,35 @@
+"""
+    rand_spin(rng::AbstractRNG = default_rng())
+
+Generate a uniformly random spin vector (normalized to 1)
+"""
 function rand_spin(rng::AbstractRNG = default_rng())
     ϕ = 2π * rand(rng)
     θ = acos(2 * rand(rng) - 1)
     return (cos(ϕ)sin(θ), sin(ϕ)sin(θ), cos(θ))
 end
 
-function init_eag!(spins::AbstractMatrix{SpinVector})
-    for I in eachindex(IndexCartesian(), spins)
-        x, y = Tuple(I)
-        spin_sign = mod(x+y, 4) < 2 ? 1.0 : -1.0
-        spins[I] = spin_sign * SVector(1.0, 0.0, 0.0)
-    end
+"""
+    spins_iter(mc::WignerMC)
+
+Returns iterable over spin-η pairs in `mc`
+"""
+function spins_iter(mc::WignerMC)
+    zip(eachslice(mc.spins, dims=(1,2)), eachslice(mc.ηs, dims=(1,2)))
 end
 
-function init_orth!(spins::AbstractMatrix{SpinVector})
-    for I in eachindex(IndexCartesian(), spins)
-        x, y = Tuple(I)
+sites_iter(mc::WignerMC) = Iterators.product(axes(mc.spins, 1), axes(mc.spins, 2))
+
+function init_orth!(mc::WignerMC)
+    for (x, y) in sites_iter(mc)
         θ = π/2 * (x + y)
-        spins[I] = SVector(cos(θ), sin(θ), 0.0)
+        mc.spins[x, y, :] = (cos(θ), sin(θ), 0.0)
     end
 end
 
-function init_afm_fe!(spins::AbstractMatrix{SpinVector}, ηs::AbstractMatrix{SpinVector})
-    for I in eachindex(IndexCartesian(), spins)
-        x, _ = Tuple(I)
-        spins[I] = SVector(0, 0, (-1)^x)
-        ηs[I] = SVector(cos(π/3), sin(π/3), 0)
+function init_afm_fe!(mc::WignerMC)
+    for (x, y) in sites_iter(mc)
+        mc.spins[x, y, :] = SVector(0, 0, (-1)^x)
+        mc.ηs[x, y, :] = SVector(cos(π/3), sin(π/3), 0)
     end
 end
