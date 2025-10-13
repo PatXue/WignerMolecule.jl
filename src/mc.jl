c6d9ad9df4a98314498eea8d9a75eaa053f9f6b4
PatXue <95881915+PatXue@users.noreply.github.com>
2025-10-13 15:20:54 -0400

    Update MC struct for annealing
    Enable higher temp in thermalization, reduced gradually for measurement

--- src/mc.jl
+++ src/mc.jl
@@ -16,8 +16,10 @@ const default_params = WignerParams(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
 const SpinVector = SVector{3, Float64}
 # Note: Using temperature in units of energy (k_B = 1)
 struct WignerMC{AlgType} <: AbstractMC
-    T::Float64     # Temperature
+    T::Float64          # Temperature
+    init_T::Float64     # Initial temperature (for thermalization)
     params::WignerParams
+
     spins::PeriodicMatrix{SpinVector}
     ηs::PeriodicMatrix{SpinVector}
 
@@ -30,12 +32,12 @@ struct WignerMC{AlgType} <: AbstractMC
     savefreq::Int  # No. of sweeps between saving local spin current
 end
 
-function WignerMC{AlgType}(; T=1.0, wigparams=default_params, Lx=40, Ly=40,
+function WignerMC{AlgType}(; T=1.0, init_T=1.0, wigparams=default_params, Lx=40, Ly=40,
     outdir=".", savefreq=0) where {AlgType}
     init_spins = fill(zeros(SpinVector), (Lx, Ly))
     init_ηs = fill(zeros(SpinVector), (Lx, Ly))
     return WignerMC{AlgType}(
-        T, wigparams, init_spins, init_ηs,
+        T, init_T=1.0, wigparams, init_spins, init_ηs,
         Array{ComplexF64}(undef, (Lx, Ly, 3)),
         Array{ComplexF64}(undef, (Lx, Ly, 3)),
         Matrix{Float64}(undef, (Lx, Ly)),
@@ -47,8 +49,9 @@ end
 function WignerMC{AlgType}(params::AbstractDict) where {AlgType}
     Lx, Ly = params[:Lx], params[:Ly]
     T = params[:T]
+    init_T = get(params, :init_T, T)
     wigparams = params[:wigparams]
     outdir = get(params, :outdir, ".")
     savefreq = get(params, :savefreq, 0)
-    return WignerMC{AlgType}(; T, wigparams, Lx, Ly, outdir, savefreq)
+    return WignerMC{AlgType}(; T, init_T, wigparams, Lx, Ly, outdir, savefreq)
 end
