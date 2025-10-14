eead421a44ffaaa9730023d05a6df85e87ecf762
PatXue <95881915+PatXue@users.noreply.github.com>
2025-10-13 21:26:19 -0400

    Create job for stripe phase

---
+++ jobs/stripe-anneal.jl
@@ -0,0 +1,33 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "stripe-anneal"
+
+L = 20
+tm.Lx = tm.Ly = L
+tm.sweeps = 20000
+tm.thermalization = 20000
+tm.binsize = 100
+
+tm.wigparams = WignerParams(load_object("all_params.jld2")[(45, 5, 20, 6)]...)
+tm.init_T = 10
+Ts = 1:0.5:5
+for T in Ts
+    tm.T = T
+    spins_dir = "$jobname.data/$(current_task_name(tm))"
+    tm.outdir = spins_dir
+    task(tm)
+end
+
+job = JobInfo("$jobname", WignerMC{:Metropolis};
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
