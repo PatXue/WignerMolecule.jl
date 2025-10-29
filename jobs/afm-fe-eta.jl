852b9ab740f808db59ebadebe9ce7f6f99a8a733
PatXue <95881915+PatXue@users.noreply.github.com>
2025-10-29 18:33:56 -0400

    Run eta-only with wide temp sweep and no thermalization

--- jobs/afm-fe-eta.jl
+++ jobs/afm-fe-eta.jl
@@ -11,13 +11,13 @@ jobname = "afm-fe-eta"
 
 L = 20
 tm.Lx = tm.Ly = L
-tm.sweeps = 60000
-tm.thermalization = 60000
+tm.sweeps = 30000
+tm.thermalization = 0
 tm.binsize = 500
 tm.init_type = :afm_fe_s
 
 tm.wigparams = WignerParams(load_object("all_params.jld2")[(45, 11, 20, 10)]...)
-Ts = 0.0:0.1:2.0
+Ts = 0.0:1.0:10.0
 for T in Ts
     # tm.thermalization = T < 0.3 ? 40000 : 20000
     tm.T = max(T, 0.01)
