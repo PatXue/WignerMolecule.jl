0147ae56cd57e926decdad99279ac8dccbb69315
PatXue <95881915+PatXue@users.noreply.github.com>
2025-10-29 21:56:09 -0400

    Helper function reduce reptition in energy/half_energy

--- src/energy.jl
+++ src/energy.jl
@@ -1,9 +1,6 @@
 const ω::ComplexF64 = exp(im * 2π/3)
-# Calculate the energy at a lattice site (x, y) if it had spin s and
-# pseudospin η
-function energy(mc::WignerMC, s::SpinVector, η::SpinVector, x, y)
-    # Nearest neighbor lattice positions
-    nns = ((x+1, y), (x+1, y-1), (x, y-1), (x-1, y), (x-1, y+1), (x, y+1))
+
+function bond_energy(mc::WignerMC, s::SpinVector, η::SpinVector, ν, pos)
     # Coupling energies
     J_SS = mc.params.J_SS
     J_EzEz_SS = mc.params.J_EzEz_SS
@@ -14,35 +11,42 @@ function energy(mc::WignerMC, s::SpinVector, η::SpinVector, x, y)
     J_EMEP = mc.params.J_EMEP
     J_EMEM = mc.params.J_EMEM
 
+    sj = mc.spins[pos...]
+    ηj = mc.ηs[pos...]
+
+    # η raising and lowering operators
+    η_m = η[1] + 1.0im*η[2]
+    ηj_p = ηj[1] - 1.0im*ηj[2]
+    ηj_m = ηj[1] + 1.0im*ηj[2]
+
+    E_spin = 0.0 + 0.0im
+    E_η = 0.0 + 0.0im
+
+    # η-only energy
+    E_η +=   J_EzEz *     η[3] * ηj[3]
+    E_η += 2*J_EMEP *     η_m * ηj_p
+    E_η += 2*J_EMEM * ν * η_m * ηj_m
+
+    # η-S energy
+    E_spin +=   J_EzEz_SS *     η[3] * ηj[3]
+    E_spin += 2*J_EMEP_SS *     η_m * ηj_p
+    E_spin += 2*J_EMEM_SS * ν * η_m * ηj_m
+    E_spin += J_SS
+    E_spin += 2*J_EAM_SS * (η_m/ν + ηj_p*ν)
+
+    return real(E_spin * (s⋅sj) + E_η)
+end
+
+# Calculate the energy at a lattice site (x, y) if it had spin s and
+# pseudospin η
+function energy(mc::WignerMC, s::SpinVector, η::SpinVector, x, y)
+    # Nearest neighbor lattice positions
+    nns = ((x+1, y), (x+1, y-1), (x, y-1), (x-1, y), (x-1, y+1), (x, y+1))
     E = 0.0
     for j in eachindex(nns)
         ν = ω^(j-1)
-        sj = mc.spins[nns[j]...]
-        ηj = mc.ηs[nns[j]...]
-
-        # η raising and lowering operators
-        η_m = η[1] + 1.0im*η[2]
-        ηj_p = ηj[1] - 1.0im*ηj[2]
-        ηj_m = ηj[1] + 1.0im*ηj[2]
-
-        E_spin = 0.0 + 0.0im
-        E_η = 0.0 + 0.0im
-
-        # η-only energy
-        E_η +=   J_EzEz *     η[3] * ηj[3]
-        E_η += 2*J_EMEP *     η_m * ηj_p
-        E_η += 2*J_EMEM * ν * η_m * ηj_m
-
-        # η-S energy
-        E_spin +=   J_EzEz_SS *     η[3] * ηj[3]
-        E_spin += 2*J_EMEP_SS *     η_m * ηj_p
-        E_spin += 2*J_EMEM_SS * ν * η_m * ηj_m
-        E_spin += J_SS
-        E_spin += 2*J_EAM_SS * (η_m/ν + ηj_p*ν)
-
-        E += real(E_spin * (s⋅sj) + E_η)
+        E += bond_energy(mc, s, η, ν, nns[j])
     end
-
     return E
 end
 
@@ -51,47 +55,11 @@ end
 function half_energy(mc::WignerMC, x, y)
     # Nearest neighbor lattice positions
     nns = ((x+1, y), (x+1, y-1), (x, y-1))
-    # Coupling energies
-    J_SS = mc.params.J_SS
-    J_EzEz_SS = mc.params.J_EzEz_SS
-    J_EzEz = mc.params.J_EzEz
-    J_EAM_SS = mc.params.J_EAM_SS
-    J_EMEP_SS = mc.params.J_EMEP_SS
-    J_EMEM_SS = mc.params.J_EMEM_SS
-    J_EMEP = mc.params.J_EMEP
-    J_EMEM = mc.params.J_EMEM
-
-    s = mc.spins[x, y]
-    η = mc.ηs[x, y]
     E = 0.0
     for j in eachindex(nns)
         ν = ω^(j-1)
-        sj = mc.spins[nns[j]...]
-        ηj = mc.ηs[nns[j]...]
-
-        # η raising and lowering operators
-        η_m = η[1] + 1.0im*η[2]
-        ηj_p = ηj[1] - 1.0im*ηj[2]
-        ηj_m = ηj[1] + 1.0im*ηj[2]
-
-        E_spin = 0.0 + 0.0im
-        E_η = 0.0 + 0.0im
-
-        # η-only energy
-        E_η +=   J_EzEz *     η[3] * ηj[3]
-        E_η += 2*J_EMEP *     η_m * ηj_p
-        E_η += 2*J_EMEM * ν * η_m * ηj_m
-
-        # η-S energy
-        E_spin +=   J_EzEz_SS *     η[3] * ηj[3]
-        E_spin += 2*J_EMEP_SS *     η_m * ηj_p
-        E_spin += 2*J_EMEM_SS * ν * η_m * ηj_m
-        E_spin += J_SS
-        E_spin += 2*J_EAM_SS * (η_m/ν + ηj_p*ν)
-
-        E += real(E_spin * (s⋅sj) + E_η)
+        E += bond_energy(mc, mc.spins[x, y], mc.ηs[x, y], ν, nns[j])
     end
-
     return E
 end
 
