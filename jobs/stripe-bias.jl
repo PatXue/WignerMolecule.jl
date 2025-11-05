387ca2fa4a7ebac57dbeba41c2019e22c20931ef
PatXue <95881915+PatXue@users.noreply.github.com>
2025-11-05 18:12:04 -0500

    Normalize stripe-bias Wignerparams

--- jobs/stripe-bias.jl
+++ jobs/stripe-bias.jl
@@ -3,6 +3,7 @@ Pkg.activate("..")
 
 using Carlo
 using Carlo.JobTools
+using LinearAlgebra
 using JLD2
 using WignerMolecule
 
@@ -10,18 +11,20 @@ tm = TaskMaker()
 jobname = "stripe-bias"
 
 tm.sweeps = 50000
-tm.thermalization = 50000
+tm.thermalization = 100000
 tm.binsize = 500
 
 stripe_bias(B) = (x, _) -> [0, 0, B * (-1)^(div(x, 2))]
 tm.bias = stripe_bias(1.0)
 bias_type = typeof(tm.bias)
 
-tm.wigparams = WignerParams(load_object("all_params.jld2")[(45, 5, 20, 6)]...)
+raw_params = load_object("all_params.jld2")[(45, 5, 20, 6)]
+norm_params = raw_params ./ norm(raw_params)
+tm.wigparams = WignerParams(norm_params...)
 tm.init_T = 10
 tm.T = 0.01
 Ls = [20]
-Bs = 0.0:2.0:20.0
+Bs = 0.0:0.5:5.0
 for L in Ls
     tm.Lx = tm.Ly = L
     for B in Bs
