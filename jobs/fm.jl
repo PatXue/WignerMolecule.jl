8e24ea417ef5f5386ec8adc8b9fce6dffd121dae
Patrick Xue <xuepat360@gmail.com>
2025-12-06 13:04:23 -0500

    Add system size sweep to fm.jl

--- jobs/fm.jl
+++ jobs/fm.jl
@@ -1,29 +1,34 @@
 import Pkg
 Pkg.activate("..")
 
-using WignerMolecule
 using Carlo
 using Carlo.JobTools
+using JLD2
+using WignerMolecule
 
 tm = TaskMaker()
+jobname = "fm"
 
-L = 40
-tm.Lx = tm.Ly = L
-tm.sweeps = 20000
-tm.thermalization = 10000
-tm.binsize = 100
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 500
 tm.init_type = :const
 
-tm.wigparams = WignerParams(-2.12742, -7.37151, -2.60026, (-1.5492-3.67457im), (2.22406+3.06319im), 0.672249, (0.215811+0.332566im), 0.815589)
-Ts = 1:0.25:5
-for T in Ts
-    tm.T = T
-    spins_dir = "fm.data/$(current_task_name(tm))"
-    tm.outdir = spins_dir
-    task(tm)
+tm.wigparams = WignerParams(load_object("all_params.jld2")[(45, 5, 20, 9)]...)
+Ts = 0.5:0.5:7
+Ls = [20, 40, 80]
+for L in Ls
+    tm.Lx = tm.Ly = L
+    tm.sweeps = 50000 * L/20
+    for T in Ts
+        tm.T = max(T, 0.01)
+        spins_dir = "$jobname.data/$(current_task_name(tm))"
+        tm.outdir = spins_dir
+        task(tm)
+    end
 end
 
-job = JobInfo("fm", WignerMC{:Metropolis, Nothing};
+job = JobInfo("$jobname", WignerMC{:Metropolis, Nothing};
     run_time = "24:00:00",
     checkpoint_time = "30:00",
     tasks = make_tasks(tm),
