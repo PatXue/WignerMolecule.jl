67f11362da1f6323a1d5f708e126caededbf5aaa
Patrick Xue <xuepat360@gmail.com>
2025-12-27 12:33:51 -0500

    Vary J+- spin coupling

--- jobs/stripe-coupl.jl
+++ jobs/stripe-coupl.jl
@@ -19,11 +19,11 @@ tm.init_type = :stripe
 
 param_d = load_object("all_params.jld2")
 tm.T = 0.1
-mults = 0.0:0.1:2.0
+mults = Iterators.flatten((0.0:0.1:1.5, 2.0:0.5:10.0),)
 for mult in mults
     tm.mult = mult
     raw_params = collect(param_d[(45, 5, 20, 7)])
-    raw_params[4] *= mult
+    raw_params[5] *= mult
     norm_params = raw_params ./ norm(raw_params)
     tm.wigparams = WignerParams(norm_params...)
 
