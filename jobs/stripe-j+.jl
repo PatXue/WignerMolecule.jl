ef6330aa34c043bf82cd5ff2c5fa8e5ba4f0c184
Patrick Xue <xuepat360@gmail.com>
2025-12-29 15:08:54 -0500

    Stripe runs with only J+ coupling

---
+++ jobs/stripe-j+.jl
@@ -0,0 +1,44 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using LinearAlgebra
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "stripe-j+"
+
+L = 40
+tm.Lx = tm.Ly = L
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 500
+tm.init_type = :stripe
+Ts = 0.0:0.2:2.0
+
+tm.J = :Re
+tm.wigparams = WignerParams(0, 0, 0, 1.0, 0, 0, 0, 0)
+for T in Ts
+    tm.T = T
+    spins_dir = "$jobname.data/$(current_task_name(tm))"
+    tm.outdir = spins_dir
+    task(tm)
+end
+
+tm.J = :Im
+tm.wigparams = WignerParams(0, 0, 0, 1.0im, 0, 0, 0, 0)
+for T in Ts
+    tm.T = T
+    spins_dir = "$jobname.data/$(current_task_name(tm))"
+    tm.outdir = spins_dir
+    task(tm)
+end
+
+job = JobInfo("$jobname", WignerMC{:Metropolis_η, Nothing};
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
