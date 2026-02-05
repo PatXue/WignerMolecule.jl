a030eb98a1e9867447b6316b93794761978886e4
Patrick Xue <xuepat360@gmail.com>
2026-02-05 15:15:22 -0500

    Increase sweeps on L=80 stripe-anneal

--- jobs/stripe-anneal.jl
+++ jobs/stripe-anneal.jl
@@ -31,7 +31,10 @@ for L in Ls
     tm.Lx = tm.Ly = L
     tm.sweeps = 100000 * div(L, 20)
     tm.thermalization = 100000 * div(L, 20)
-    tm.binsize = div(tm.sweeps, 100)
+    if L == 80
+        tm.sweeps = 2 * tm.sweeps
+    end
+    tm.binsize = div(tm.thermalization, 100)
     for T in Ts
         tm.T = T
         spins_dir = "$jobname.data/$(current_task_name(tm))"
