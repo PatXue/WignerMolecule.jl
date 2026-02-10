d5e75df68b9e978ac1a773806062dae5f6b5d758
Patrick Xue <xuepat360@gmail.com>
2026-02-10 11:44:14 -0500

    Rerun fm-bias

--- jobs/fm-bias.jl
+++ jobs/fm-bias.jl
@@ -4,31 +4,35 @@ Pkg.activate("..")
 using Carlo
 using Carlo.JobTools
 using JLD2
+using JSON
 using WignerMolecule
 
 tm = TaskMaker()
 jobname = "fm-bias"
 
-tm.sweeps = 100000
-tm.thermalization = 100000
-tm.binsize = 1000
-tm.init_type = :rand
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 500
+tm.init_type = :const
 
-fm_bias(B) = (_, _) -> [0, 0, B]
-tm.bias = fm_bias(1.0)
-bias_type = typeof(tm.bias)
+fm_bias(_, _) = [0, 0, 1]
+tm.bias = fm_bias
+bias_type = typeof(fm_bias)
+tm.init_B = 10.0
+JSON.lower(f::bias_type) = f(1, 1)
 
 tm.wigparams = WignerParams(load_object("all_params.jld2")[(45, 5, 20, 9)]...)
-tm.init_T = 10
-tm.T = 0.5
-tm.Lx = tm.Ly = 80
+tm.Lx = tm.Ly = 20
+Ts = [4.5, 5.5, 6.5]
 Bs = 0.0:1.0:10.0
-for B in Bs
-    tm.B = B
-    tm.bias = fm_bias(B)
-    spins_dir = "$jobname.data/$(current_task_name(tm))"
-    tm.outdir = spins_dir
-    task(tm)
+for T in Ts
+    tm.T = T
+    for B in Bs
+        tm.B = B
+        spins_dir = "$jobname.data/$(current_task_name(tm))"
+        tm.outdir = spins_dir
+        task(tm)
+    end
 end
 
 job = JobInfo("$jobname", WignerMC{:Metropolis, bias_type};
