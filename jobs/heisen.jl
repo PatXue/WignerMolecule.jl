a1d62455f63af3c073f4fd9a125bcbbbf023ecc3
Patrick Xue <xuepat360@gmail.com>
2026-02-20 13:24:36 -0500

    Run heisenberg with zero field over temperature

---
+++ jobs/heisen.jl
@@ -0,0 +1,43 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using JSON
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "heisen"
+tm.init_type = :const
+
+fm_bias(_, _) = [0, 0, 1]
+tm.bias = fm_bias
+bias_type = typeof(fm_bias)
+tm.init_B = 10.0
+tm.B = 0.0
+JSON.lower(f::bias_type) = f(1, 1)
+
+tm.wigparams = WignerParams(-1, 0, 0, 0, 0, 0, 0, 0)
+tm.Lx = tm.Ly = 20
+Ls = [20]
+Ts = 0.1:0.1:2.0
+for L in Ls
+    tm.Lx = tm.Ly = L
+    tm.sweeps = 25000 * div(L, 20)
+    tm.thermalization = tm.sweeps
+    tm.binsize = div(tm.sweeps, 100)
+    for T in Ts
+        tm.T = T
+        spins_dir = "$jobname.data/$(current_task_name(tm))"
+        tm.outdir = spins_dir
+        task(tm)
+    end
+end
+
+job = JobInfo("$jobname", WignerMC{:Metropolis_s, bias_type};
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
