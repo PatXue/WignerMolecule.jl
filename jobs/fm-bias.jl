0c9758af7dcfdbf05282e7d4c9ac88fb23074bb7
Patrick Xue <xuepat360@gmail.com>
2026-02-20 23:23:31 -0500

    Sweep system size in fm-bias

--- jobs/fm-bias.jl
+++ jobs/fm-bias.jl
@@ -9,10 +9,6 @@ using WignerMolecule
 
 tm = TaskMaker()
 jobname = "fm-bias"
-
-tm.sweeps = 50000
-tm.thermalization = 50000
-tm.binsize = 500
 tm.init_type = :const
 
 fm_bias(_, _) = [0, 0, 1]
@@ -23,15 +19,22 @@ JSON.lower(f::bias_type) = f(1, 1)
 
 tm.wigparams = WignerParams(load_object("all_params.jld2")[(45, 5, 20, 9)]...)
 tm.Lx = tm.Ly = 20
+Ls = [20, 40, 80]
 Ts = [4.5, 5.5, 6.5]
 Bs = 0.0:1.0:10.0
-for T in Ts
-    tm.T = T
-    for B in Bs
-        tm.B = B
-        spins_dir = "$jobname.data/$(current_task_name(tm))"
-        tm.outdir = spins_dir
-        task(tm)
+for L in Ls
+    tm.Lx = tm.Ly = L
+    tm.sweeps = 50000 * div(L, 20)
+    tm.thermalization = tm.sweeps
+    tm.binsize = div(tm.sweeps, 100)
+    for T in Ts
+        tm.T = T
+        for B in Bs
+            tm.B = B
+            spins_dir = "$jobname.data/$(current_task_name(tm))"
+            tm.outdir = spins_dir
+            task(tm)
+        end
     end
 end
 
