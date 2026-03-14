e3cbc389de46e60599816318a3c0d3322c1d73a2
PatXue <95881915+PatXue@users.noreply.github.com>
2026-03-13 20:21:05 -0400

    Construct WignerMC in stripe.jl

--- skyrmions/stripe.jl
+++ skyrmions/stripe.jl
@@ -2,5 +2,19 @@ import Pkg
 Pkg.activate("..")
 
 using JLD2
+using JSON
+using LinearAlgebra
 using WignerMolecule
 
+stripe_bias(x, _) = [0, 0, (-1)^(div(x, 2))]
+bias = stripe_bias
+bias_type = typeof(stripe_bias)
+JSON.lower(f::bias_type) = f(1, 1)
+
+Lx = Ly = 40
+
+raw_params = load_object("all_params.jld2")[(45, 5, 20, 6)]
+norm_params = raw_params ./ norm(raw_params)
+wigparams = WignerParams(norm_params...)
+
+mc = WignerMC{:None, bias_type}(; wigparams, bias, Lx, Ly)
