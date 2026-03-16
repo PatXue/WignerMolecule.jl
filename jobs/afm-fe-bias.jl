eae6c831eae9f9e1df9ae2530c61c369b04cb2f0
Patrick Xue <xuepat360@gmail.com>
2026-03-15 23:39:20 -0400

    Set init_T to 0 in afm-fe-bias, originally forgot to remove

--- jobs/afm-fe-bias.jl
+++ jobs/afm-fe-bias.jl
@@ -15,13 +15,12 @@ tm.init_type = :afm_fe
 afm_bias(x, _) = [0, 0, (-1)^x]
 tm.bias = afm_bias
 bias_type = typeof(afm_bias)
-tm.init_B = 10.0
+tm.init_B = 5.0
 JSON.lower(f::bias_type) = f(1, 1)
 
 raw_params = load_object("all_params.jld2")[(45, 11, 20, 10)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-tm.init_T = 10
 Ls = [20, 40, 80]
 Ts = [0.2, 0.3, 0.4]
 Bs = 0.0:0.04:0.4
