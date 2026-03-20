772965a343244938236dfc43568aec56394b3823
PatXue <95881915+PatXue@users.noreply.github.com>
2026-03-19 21:44:48 -0400

    Change bias and wigner parameters for stripe

--- skyrmions/stripe.jl
+++ skyrmions/stripe.jl
@@ -6,14 +6,14 @@ using JSON
 using LinearAlgebra
 using WignerMolecule
 
-fm_bias(_, _) = [0, 0, 1]
-bias = fm_bias
-bias_type = typeof(fm_bias)
+stripe_bias(x, _) = [0, 0, (-1)^(div(x, 2))]
+bias = stripe_bias
+bias_type = typeof(stripe_bias)
 
 Lx = Ly = 40
 B = 0.0
 
-raw_params = load_object("all_params.jld2")[(45, 5, 20, 9)]
+raw_params = load_object("all_params.jld2")[(45, 5, 20, 6)]
 norm_params = raw_params ./ norm(raw_params)
 wigparams = WignerParams(norm_params...)
 
