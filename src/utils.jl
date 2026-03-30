dcfb86cbec1f6253f3f4c58da84103452bfe31ec
PatXue <95881915+PatXue@users.noreply.github.com>
2026-03-30 12:14:26 -0400

    Add missing indices in K and half_K

--- src/utils.jl
+++ src/utils.jl
@@ -159,12 +159,12 @@ end
 function K(corrs)
     Lx, _ = size(corrs)
     n = div(Lx, 3)
-    return corrs[2n+1,n+1]
+    return corrs[2n+1,n+1,:]
 end
 function half_K(corrs)
     Lx, _ = size(corrs)
     n = div(Lx, 6)
-    return corrs[2n+1,n+1]
+    return corrs[2n+1,n+1,:]
 end
 
 # Calculate skyrmion number
