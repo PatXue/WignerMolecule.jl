a2fdbaeca13ebda4e8150e9eaf9505fab797aaa9
Patrick Xue <xuepat360@gmail.com>
2026-04-08 10:27:13 -0400

    Lower initial bias afm afe

--- jobs/afm-fe-anneal.jl
+++ jobs/afm-fe-anneal.jl
@@ -16,7 +16,7 @@ afm_bias(x, _) = [0, 0, (-1)^x]
 tm.bias = afm_bias
 bias_type = typeof(afm_bias)
 tm.B = 0.0
-tm.init_B = 10.0
+tm.init_B = 1.0
 JSON.lower(f::bias_type) = f(1, 1)
 
 raw_params = load_object("all_params.jld2")[(45, 11, 20, 10)]
@@ -24,7 +24,7 @@ norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
 Ts = 0.05:0.05:0.6
 Ls = [20, 40, 80]
-for (L, T) in Iterators.product(Ls, Ts)
+for (T, L) in Iterators.product(Ts, Ls)
     tm.Lx = tm.Ly = L
     tm.sweeps = 50000 * div(L, 20)
     tm.thermalization = (T <= 0.4 ? 2 : 1) * tm.sweeps
