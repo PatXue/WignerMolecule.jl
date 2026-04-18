dde1c9ce6619cd53991709833cad87952ff34bca
PatXue <95881915+PatXue@users.noreply.github.com>
2026-04-18 17:38:00 -0400

    Add sample! to HighTemp to sample measureables on mc

--- src/HighTemp.jl
+++ src/HighTemp.jl
@@ -1,13 +1,25 @@
 # Sampling functions for high-T expectations
 module HighTemp
 
-# Randomize spins in MC, updates Fourier transform if flag passed
-function randomize!(mc::WignerMC; up_fourier=false)
+export randomize!
+
+# Randomize spins in MC
+function randomize!(mc::WignerMC)
     rand!(mc.spins)
     rand!(mc.ηs)
-    if up_fourier
-        update_fourier!(mc)
+end
+
+# Sample values of functions in fs over n runs on random spins
+function sample!(mc::WignerMC, n, fs::Vararg{Function, N}) where {N}
+    means = fill(0.0, N)
+    errs = fill(0.0, N)
+    for _ in 1:n
+        randomize!(mc)
+        vals = [f(mc) for f in fs]
+        @. means = means + vals
+        @. errs = errs + vals^2
     end
+    @. return (means / n, sqrt((errs - means) / (n-1)))
 end
 
 end
