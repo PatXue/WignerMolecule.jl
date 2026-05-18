3e8d694eff7397ddc55a501b275a9bdd16f88796
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-18 11:57:19 -0400

    Set up main sampling loop in stripe

--- high-temp/stripe.jl
+++ high-temp/stripe.jl
@@ -4,8 +4,19 @@ Pkg.activate(@__DIR__)
 using JLD2
 using WignerMolecule
 
-Lx = Ly = 6
+norm2(v) = sum(abs2.(v))
+
+Lx = Ly = L = 8
 raw_params = load_object("all_params.jld2")[(45, 5, 20, 6)]
 norm_params = raw_params ./ norm(raw_params)
 wigparams = WignerParams(norm_params...)
 mc = WignerMC{:HighTemp, Nothing}(; Lx, Ly, wigparams, bias=nothing)
+
+n = 10^3
+ord = 4
+for _ in 1:n
+    randomize!(mc)
+    E = total_energy(mc)
+    sk = mc.spinks[div(L, 4)+1, 1, :]
+    ηk = mc.ηks[div(Lx, 2)+1, 1, :]
+end
