a26a3ad92c8b7e56e4dea8b93020519cb18d81c0
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-18 13:28:54 -0400

    Write addsample to add a sample to an expectation

--- high-temp/Expectation.jl
+++ high-temp/Expectation.jl
@@ -22,4 +22,17 @@ function combine(e1::Expectation, e2::Expectation)
     return Expectation(val, err, n1+n2)
 end
 
+function addsample(e::Expectation, x)
+    if e.n == 0
+        return Expectation(x, 0.0, 1)
+    end
+    val = e.val
+    err = e.err
+    n = e.n
+    new_val = (n*val + x) / (n+1)
+    old_sqs = (n-1) * err^2 + val^2  # Avg of squares for old samples
+    new_sqs = (n*old_sqs + x^2) / (n+1)
+    return Expectation(new_val, sqrt((new_sqs - new_val^2) / n), n+1)
+end
+
 end
