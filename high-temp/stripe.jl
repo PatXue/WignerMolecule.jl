cbc8b26f9a93ea4b7fdd9604718e27957f2924c4
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-18 11:27:46 -0400

    Initialize MC

--- high-temp/stripe.jl
+++ high-temp/stripe.jl
@@ -3,3 +3,9 @@ Pkg.activate(@__DIR__)
 
 using JLD2
 using WignerMolecule
+
+Lx = Ly = 6
+raw_params = load_object("all_params.jld2")[(45, 5, 20, 6)]
+norm_params = raw_params ./ norm(raw_params)
+wigparams = WignerParams(norm_params...)
+mc = WignerMC{:HighTemp, Nothing}(; Lx, Ly, wigparams, bias=nothing)
