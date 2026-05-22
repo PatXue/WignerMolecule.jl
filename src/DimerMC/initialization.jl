c5dde543aabda8bd7c1121c5939e0bc1618ee284
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-22 18:49:09 -0400

    Initialize mc spins in vbs phase

--- src/DimerMC/initialization.jl
+++ src/DimerMC/initialization.jl
@@ -1,10 +1,22 @@
 # Initialize the DimerMC spins/etas
+const unitcell = [a2 a6; a5 a3]
 function Carlo.init!(mc::DimerMC, ctx::Carlo.MCContext, params::AbstractDict)
+    for I in eachindex(IndexCartesian(), mc.spins)
+        x, y = Tuple(I)
+        # Shift x,y to be inside unit cell
+        while y > 2
+            x += 1
+            y -= 2
+        end
+        x = (x % 2) + 1
+
+        mc.spins[I] = unitcell[x,y]
+    end
+
     init_type::Symbol = get(params, :init_type, :rand)
     rand!(ctx.rng, mc.ηs)
     if init_type == :const
-        for I in eachindex(mc.spins)
-            mc.spins[I] = SpinVector(0, 0, 1)
+        for I in eachindex(mc.ηs)
             mc.ηs[I] = SpinVector(0, 0, 1)
         end
     elseif init_type == :afm_fe_eta
