a1de208d495949b526e395b018fb45b27927e879
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-22 11:53:43 -0400

    Copy afm-fe-anneal to afm-fe-high-t

---
+++ jobs/afm-fe-high-t.jl
@@ -0,0 +1,41 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using JSON
+using LinearAlgebra
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "afm-fe-anneal"
+tm.init_type = :afm_fe
+
+afm_bias(x, _) = [0, 0, (-1)^x]
+tm.bias = afm_bias
+bias_type = typeof(afm_bias)
+tm.B = 0.0
+tm.init_B = 1.0
+JSON.lower(f::bias_type) = f(1, 1)
+
+raw_params = load_object("all_params.jld2")[(45, 11, 20, 10)]
+norm_params = raw_params ./ norm(raw_params)
+tm.wigparams = WignerParams(norm_params...)
+Ts = 0.05:0.05:0.6
+Ls = [20, 40, 80]
+for (T, L) in Iterators.product(Ts, Ls)
+    tm.Lx = tm.Ly = L
+    tm.sweeps = 100000 * div(L, 20)
+    tm.thermalization = (T <= 0.4 ? 2 : 1) * 50000 * div(L, 20)
+    tm.binsize = div(50000 * div(L,20), 100)
+    tm.T = max(T, 0.01)
+    task(tm)
+end
+
+job = JobInfo("$jobname", WignerMC{:Metropolis, bias_type};
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
