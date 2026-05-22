130a6abe60d7faa7dbed2d4bad68a595d4d295ed
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-22 16:49:50 -0400

    DimerMC constructors

--- src/DimerMC/mc.jl
+++ src/DimerMC/mc.jl
@@ -11,3 +11,25 @@ struct DimerMC <: AbstractMC
     outdir::String # Output directory for spin plots
     savefreq::Int  # No. of sweeps between saving spins
 end
+
+function DimerMC(; T, init_T, wigparams, Lx, Ly, outdir="", savefreq=0)
+    init_ss = fill((0,0), (Lx, Ly))
+    init_ηs = fill(zeros(SpinVector), (Lx, Ly))
+    return DimerMC(
+        T, init_T, wigparams, init_ss, init_ηs,
+        Array{ComplexF64}(undef, (Lx, Ly, 3)),
+        outdir, savefreq
+    )
+end
+
+function DimerMC(params::AbstractDict)
+    Lx, Ly = params[:Lx], params[:Ly]
+    T = params[:T]
+    init_T = get(params, :init_T, T)
+    wigparams = params[:wigparams]
+
+    outdir = get(params, :outdir, ".")
+    savefreq = get(params, :savefreq, 0)
+
+    return DimerMC(; T, init_T, wigparams, Lx, Ly, outdir, savefreq)
+end
