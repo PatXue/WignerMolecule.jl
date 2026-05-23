731eb53a220c502cdad1765c70624937fa4ef690
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-23 11:35:15 -0400

    Copy common.jl into DimerMC

---
+++ src/DimerMC/common.jl
@@ -0,0 +1,83 @@
+function Carlo.measure!(mc::WignerMC, ctx::Carlo.MCContext)
+    Lx, Ly = size(mc.spins)
+    N = Lx * Ly
+    # Magnetization per lattice site
+    mag_v = sum(mc.spins) ./ N
+    mag = norm(mag_v)
+    measure!(ctx, :Mag, mag)
+    measure!(ctx, :Mag2, mag^2)
+    measure!(ctx, :Magx, mag_v[1])
+    measure!(ctx, :Magy, mag_v[2])
+    measure!(ctx, :Magz, mag_v[3])
+
+    η = sum(mc.ηs) ./ N
+    measure!(ctx, :ηx, η[1])
+    measure!(ctx, :ηy, η[2])
+    measure!(ctx, :ηz, abs(η[3]))
+    measure!(ctx, :ηxy, sqrt(η[1]^2 + η[2]^2))
+
+    # Energy per lattice site
+    E = total_energy(mc, mc.B) / N
+    measure!(ctx, :Energy, E)
+    measure!(ctx, :Energy2, E^2)
+
+    update_fourier!(mc)
+    for f in (Γ, M, M2, M3, half_M, half_K)
+        s = f(mc.spinks)
+        η = f(mc.ηks)
+        measure!(ctx, Symbol("sk_", f), s)
+        measure!(ctx, Symbol("ηk_", f), η)
+        measure!(ctx, Symbol("sk_corr_", f), norm2(s))
+        measure!(ctx, Symbol("ηk_corr_", f), η*η')
+    end
+
+    Q = calc_Q(mc.spins) / N
+    measure!(ctx, :Q, Q)
+    measure!(ctx, :Q2, Q^2)
+
+    if is_save_sweep(mc, ctx)
+        save_spin(mc, ctx)
+    end
+
+    return nothing
+end
+
+function Carlo.register_evaluables(::Type{WignerMC{AlgType, BiasType}}, eval::AbstractEvaluator,
+                                   params::AbstractDict) where {AlgType, BiasType}
+    T = params[:T]
+    N = params[:Lx] * params[:Ly]
+    evaluate!(eval, :χ, (:Mag, :Mag2)) do mag, mag2
+        return N / T * (mag2 - mag^2)
+    end
+
+    evaluate!(eval, :ChiEtaxy, (:ηk_corr_Γ, :ηk_Γ)) do eta2, eta
+        return N * real(eta2[1,1] + eta2[2,2] - abs2(eta[1]) - abs2(eta[2])) / T
+    end
+    evaluate!(eval, :ChiEtaz, (:ηk_corr_Γ, :ηk_Γ)) do eta2, eta
+        return N * real(eta2[3,3] - abs2(eta[3])) / T
+    end
+
+    evaluate!(eval, :HeatCap, (:Energy2, :Energy)) do E2, E
+        return N * (E2 - E^2) / T^2
+    end
+
+    evaluate!(eval, :ChiQ, (:Q2, :Q)) do Q2, Q
+        return N * (Q2 - Q^2)
+    end
+
+    return nothing
+end
+
+function Carlo.write_checkpoint(mc::WignerMC, out::HDF5.Group)
+    out["spins"] = mc.spins
+    out["etas"] = mc.ηs
+    return nothing
+end
+function Carlo.read_checkpoint!(mc::WignerMC, in::HDF5.Group)
+    raw_spins = read(in, "spins")
+    raw_ηs = read(in, "etas")
+    mc.spins .= map(v -> SVector(v[:data][1], v[:data][2], v[:data][3]), raw_spins)
+    mc.ηs .= map(v -> SVector(v[:data][1], v[:data][2], v[:data][3]), raw_ηs)
+    return nothing
+end
+
