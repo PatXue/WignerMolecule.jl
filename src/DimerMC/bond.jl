ff243df12a6119f72b19cc8685d427116ba5db67
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-25 19:20:07 -0400

    Create Dimer struct to represent an entangled pair

--- src/DimerMC/bond.jl
+++ src/DimerMC/bond.jl
@@ -1,5 +1,11 @@
 # Helper functions for handling Bonds
 
+struct Dimer
+    x::Int,
+    y::Int,
+    b::Bond
+end
+
 # Bond type to index displacement
 const bondtodisp = Dict(
     a1 => (1,0),
@@ -12,9 +18,11 @@ const bondtodisp = Dict(
 
 # Get position index of (x,y)'s entanglement partner
 getpartner(mc::DimerMC, (x, y)) = (x, y) .+ bondtodisp[mc.spins[x, y]]
+getpartner(d::Dimer) = (d.x, d.y) .+ bondtodisp(d.b)
 
-# Rotate x hat to the given bond
+# Perform a rotation of x hat to lie along given bond r
 rotate(b::Bond, r::Bond) = Bond((Int(b) + Int(r)) % 6)
 function rotate((x, y), r::Bond)
     x .* bondtodisp[r] .+ y .* bondtodisp[rotate(a2, r)]
 end
+rotate(d::Dimer, r::Bond) = Dimer(rotate((d.x,d.y), r)..., rotate(d.b, r))
