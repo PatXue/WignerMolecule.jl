fc94e2fd1db43dfd2e242ebb25626a2a21ff3a39
Patrick Xue <xuepat360@gmail.com>
2026-05-25 19:15:58 -0400

    Rerun fm with lower temperature

--- jobs/fm.jl
+++ jobs/fm.jl
@@ -5,10 +5,11 @@ using Carlo
 using Carlo.JobTools
 using JLD2
 using JSON
+using LinearAlgebra
 using WignerMolecule
 
 tm = TaskMaker()
-jobname = "fm"
+jobname = "fm-anneal"
 
 tm.sweeps = 50000
 tm.thermalization = 50000
@@ -18,18 +19,20 @@ tm.init_type = :const
 fm_bias(_, _) = [0, 0, 1]
 tm.bias = fm_bias
 bias_type = typeof(fm_bias)
-tm.init_B = 20.0
+tm.init_B = 5.0
 tm.B = 0
 JSON.lower(f::bias_type) = f(1, 1)
 
-tm.wigparams = WignerParams(load_object("all_params.jld2")[(45, 5, 20, 9)]...)
-Ts = 0.5:0.5:7
-Ls = [20, 40, 80]
+raw_params = load_object("all_params.jld2")[(45, 5, 20, 9)]
+norm_params = raw_params ./ norm(raw_params)
+tm.wigparams = WignerParams(norm_params...)
+Ts = 0.02:0.02:0.4
+Ls = [20]
 for L in Ls
     tm.Lx = tm.Ly = L
-    tm.sweeps = 50000 * L/20
+    tm.sweeps = 50000 * div(L, 20)
     for T in Ts
-        tm.T = max(T, 0.01)
+        tm.T = T
         spins_dir = "$jobname.data/$(current_task_name(tm))"
         tm.outdir = spins_dir
         task(tm)
