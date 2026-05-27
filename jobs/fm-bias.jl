a471e0cfab33f5165037e6cd3dbea95f6ca1ef86
Patrick Xue <xuepat360@gmail.com>
2026-05-27 11:37:14 -0400

    Rerun L=80 system size for fm-bias

--- jobs/fm-bias.jl
+++ jobs/fm-bias.jl
@@ -21,7 +21,7 @@ JSON.lower(f::bias_type) = f(1, 1)
 raw_params = load_object("all_params.jld2")[(45, 5, 20, 9)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-Ls = [20, 40]
+Ls = [20, 40, 80]
 Ts = [0.1, 0.2, 0.3, 0.4, 0.5]
 Bs = 0.0:0.1:1.0
 for L in Ls
