ad7fc37dfd0fb79b6ffb370c24054d54a3a6f299
Patrick Xue <xuepat360@gmail.com>
2026-05-27 14:02:11 -0400

    Rerun afm-fe-eta on corrected spin couplings

--- jobs/afm-fe-eta.jl
+++ jobs/afm-fe-eta.jl
@@ -13,17 +13,16 @@ jobname = "afm-fe-eta"
 L = 20
 tm.Lx = tm.Ly = L
 tm.sweeps = 30000
-tm.thermalization = 10000
+tm.thermalization = 20000
 tm.binsize = 500
 tm.init_type = :afm_fe_s
 
 raw_params = load_object("all_params.jld2")[(45, 11, 20, 10)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-Ts = 0.0:0.5:9.5
+Ts = 0.1:0.1:1.0
 for T in Ts
-    # tm.thermalization = T < 0.3 ? 40000 : 20000
-    tm.T = max(T, 0.1)
+    tm.T = T
     spins_dir = "$jobname.data/$(current_task_name(tm))"
     tm.outdir = spins_dir
     task(tm)
