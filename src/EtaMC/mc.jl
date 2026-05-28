54554b88087bb32e62f32b88963888e99e5a8c90
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-28 12:28:47 -0400

    Constructors for EtaMC

--- src/EtaMC/mc.jl
+++ src/EtaMC/mc.jl
@@ -6,9 +6,20 @@ end
 
 struct EtaMC <: AbstractMC
     T::Float64          # Temperature
-    params::EtaParams
+    wigparams::EtaParams
 
     Ss::PeriodicMatrix{SpinVector}  # Spins
     Sks::Array{ComplexF64, 3}       # Fourier transformed spins
 end
 
+function EtaMC(; T=1.0, wigparams, Lx=20, Ly=20)
+    init = fill(zeros(SpinVector), (Lx, Ly))
+    return EtaMC(T, wigparams, init, Array{ComplexF64, 3}(undef, (Lx, Ly, 3)))
+end
+
+function EtaMC(params::AbstractDict)
+    Lx, Ly = params[:Lx], params[:Ly]
+    T = params[:T]
+    wigparams = params[:wigparams]
+    return EtaMC(; T, Lx, Ly, wigparams)
+end
