f7e249b71a38f9008e6ec6c29c84b97e74d9bbef
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-28 12:56:03 -0400

    Measure function for EtaMC

--- src/EtaMC/common.jl
+++ src/EtaMC/common.jl
@@ -22,3 +22,34 @@ function Carlo.init!(mc::EtaMC, ctx::Carlo.MCContext, params::AbstractDict)
     return nothing
 end
 
+function Carlo.measure!(mc::EtaMC, ctx::Carlo.MCContext)
+    Lx, Ly = size(mc.spins)
+    N = Lx * Ly
+    # Site spin expectations
+    mag_v = sum(mc.spins) ./ N
+    mag = norm(mag_v)
+    measure!(ctx, :Mag, mag)
+    measure!(ctx, :Mag2, mag^2)
+    measure!(ctx, :Magx, mag_v[1])
+    measure!(ctx, :Magy, mag_v[2])
+    measure!(ctx, :Magz, mag_v[3])
+
+    # Energy per lattice site
+    E = total_energy(mc, mc.B) / N
+    measure!(ctx, :Energy, E)
+    measure!(ctx, :Energy2, E^2)
+
+    update_fourier!(mc)
+    for f in (Γ, M, M2, M3)
+        s = f(mc.spinks)
+        measure!(ctx, Symbol("sk_", f), s)
+        measure!(ctx, Symbol("sk_corr_", f), s*s')
+    end
+
+    Q = chirality(mc.spins) / N
+    measure!(ctx, :Q, Q)
+    measure!(ctx, :Q2, Q^2)
+
+    return nothing
+end
+
