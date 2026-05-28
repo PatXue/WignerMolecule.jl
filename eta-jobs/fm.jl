23ae63a6a1021784b7b493b6140494b0cecba9b2
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-28 19:38:38 -0400

    Create tasks for fm EtaMC job

--- eta-jobs/fm.jl
+++ eta-jobs/fm.jl
@@ -2,3 +2,15 @@ import Pkg
 Pkg.activate("..")
 
 using WignerMolecule
+
+tm = TaskMaker()
+jobname = "fm"
+tm.wigparams = EtaParams(1.5, 0.5)
+
+Ls = [20]
+Ts = 0.1:0.1:1.0
+for (T, L) in Iterators.product(Ts, Ls)
+    tm.T = T
+    tm.Lx = tm.Ly = L
+    task(tm)
+end
