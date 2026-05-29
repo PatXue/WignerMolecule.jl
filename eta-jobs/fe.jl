3d8e2a6eeb4af0e6e68b62da191e7a4cbe67c4a7
PatXue <95881915+PatXue@users.noreply.github.com>
2026-05-29 13:14:22 -0400

    Create FE job for EtaMC

---
+++ eta-jobs/fe.jl
@@ -0,0 +1,28 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "fe"
+tm.wigparams = EtaParams(0.5, 0.5)
+
+Ls = [20]
+Ts = 0.1:0.1:1.0
+for (T, L) in Iterators.product(Ts, Ls)
+    tm.sweeps = 20000
+    tm.thermalization = 20000
+    tm.binsize = 100
+    tm.T = T
+    tm.Lx = tm.Ly = L
+    task(tm)
+end
+
+job = JobInfo("$jobname", EtaMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
