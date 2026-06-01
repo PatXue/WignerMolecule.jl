dd877a25ef8285eac2577c5ef7f7f3b3af0e0343
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-01 14:40:25 -0400

    Rerun afm-afe-left w new norm

--- jobs/afm-afe-left.jl
+++ jobs/afm-afe-left.jl
@@ -22,11 +22,11 @@ JSON.lower(f::bias_type) = f(1, 1)
 raw_params = load_object("all_params.jld2")[(45, 11, 20, 7)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-Ts = 0.05:0.05:0.6
+Ts = 0.05:0.05:0.5
 Ls = [20]
 for L in Ls
     tm.Lx = tm.Ly = L
-    tm.sweeps = 100000 * div(L, 20)
+    tm.sweeps = 50000 * div(L, 20)
     tm.thermalization = tm.sweeps
     tm.binsize = div(tm.sweeps, 100)
     for T in Ts
