bcea4c3c9eec2d6e6ee63592eee4ac9dfe766764
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-01 18:32:20 -0400

    Lower temp range on afm-afe-left

--- jobs/afm-afe-left.jl
+++ jobs/afm-afe-left.jl
@@ -22,7 +22,7 @@ JSON.lower(f::bias_type) = f(1, 1)
 raw_params = load_object("all_params.jld2")[(45, 11, 20, 7)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-Ts = 0.05:0.05:0.5
+Ts = 0.005:0.005:0.05
 Ls = [20]
 for L in Ls
     tm.Lx = tm.Ly = L
