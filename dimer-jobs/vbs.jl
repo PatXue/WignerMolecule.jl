93abc943a5193da208f3942daa78da93f8b189d7
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-05 16:37:20 -0400

    Create job script for vbs phase

---
+++ dimer-jobs/vbs.jl
@@ -0,0 +1,33 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using LinearAlgebra
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "vbs"
+
+raw_params = load_object("all_params.jld2")[(45, 11, 20, 6)]
+norm_params = raw_params ./ norm(raw_params)
+tm.wigparams = WignerParams(norm_params...)
+
+Ts = 0.01:0.01:0.1
+Ls = [20]
+for (T, L) in Iterators.product(Ts, Ls)
+    tm.sweeps = 50000 * div(L, 20)
+    tm.thermalization = tm.sweeps
+    tm.binsize = div(tm.sweeps, 100)
+    tm.Lx = tm.Ly = L
+    tm.T = T
+    task(tm)
+end
+
+job = JobInfo("$jobname", DimerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
