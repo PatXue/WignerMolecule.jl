a77be8b47e40376e83e3bdf130f7160bf5799bf3
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-05 12:39:03 -0400

    Edit measure and checkpointing functions for DimerMC

--- src/DimerMC/common.jl
+++ src/DimerMC/common.jl
@@ -9,7 +9,7 @@ function Carlo.measure!(mc::DimerMC, ctx::Carlo.MCContext)
     measure!(ctx, :ηxy, sqrt(η[1]^2 + η[2]^2))
 
     # Energy per lattice site
-    E = total_energy(mc, mc.B) / N
+    E = total_energy(mc) / N
     measure!(ctx, :Energy, E)
     measure!(ctx, :Energy2, E^2)
 
@@ -19,38 +19,26 @@ function Carlo.measure!(mc::DimerMC, ctx::Carlo.MCContext)
         measure!(ctx, Symbol("ηk_", f), η)
         measure!(ctx, Symbol("ηk_corr_", f), η*η')
     end
-
-    if is_save_sweep(mc, ctx)
-        save_spin(mc, ctx)
-    end
-
     return nothing
 end
 
 function Carlo.register_evaluables(::Type{DimerMC}, eval::AbstractEvaluator, params::AbstractDict)
     T = params[:T]
     N = params[:Lx] * params[:Ly]
-    evaluate!(eval, :ChiEtaxy, (:ηk_corr_Γ, :ηk_Γ)) do eta2, eta
-        return N * real(eta2[1,1] + eta2[2,2] - abs2(eta[1]) - abs2(eta[2])) / T
-    end
-    evaluate!(eval, :ChiEtaz, (:ηk_corr_Γ, :ηk_Γ)) do eta2, eta
-        return N * real(eta2[3,3] - abs2(eta[3])) / T
-    end
-
     evaluate!(eval, :HeatCap, (:Energy2, :Energy)) do E2, E
         return N * (E2 - E^2) / T^2
     end
-
     return nothing
 end
 
 function Carlo.write_checkpoint(mc::DimerMC, out::HDF5.Group)
-    out["spins"] = Int.(mc.spins)
+    out["spins"] = mc.spins
     out["etas"] = mc.ηs
     return nothing
 end
 function Carlo.read_checkpoint!(mc::DimerMC, in::HDF5.Group)
-    mc.spins .= Bond.(read(in, "spins"))
+    raw_ss = read(in, "spins")
+    mc.spins .= map(v -> SVector(v[:data][1], v[:data][2]), raw_ss)
     raw_ηs = read(in, "etas")
     mc.ηs .= map(v -> SVector(v[:data][1], v[:data][2], v[:data][3]), raw_ηs)
     return nothing
