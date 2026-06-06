66f38230596c255ec6275d206a8602f1804ed849
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-06 17:36:01 -0400

    Run fm-high-t for intermed temps as well

--- jobs/fm-high-t.jl
+++ jobs/fm-high-t.jl
@@ -4,41 +4,34 @@ Pkg.activate("..")
 using Carlo
 using Carlo.JobTools
 using JLD2
-using JSON
 using LinearAlgebra
 using WignerMolecule
 
 tm = TaskMaker()
 jobname = "fm-high-t"
 
-tm.sweeps = 20000
-tm.thermalization = 20000
-tm.binsize = 100
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 500
 tm.init_type = :const
-
-fm_bias(_, _) = [0, 0, 1]
-tm.bias = fm_bias
-bias_type = typeof(fm_bias)
-tm.init_B = 20.0
-tm.B = 0
-JSON.lower(f::bias_type) = f(1, 1)
+tm.bias = nothing
 
 raw_params = load_object("all_params.jld2")[(45, 5, 20, 9)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-Ts = 0.5:0.1:2.4
+Ts = Iterators.flatten((0.05:0.025:0.1, 0.2:0.1:2.0))
 Ls = [8]
 for L in Ls
     tm.Lx = tm.Ly = L
     for T in Ts
-        tm.T = max(T, 0.01)
+        tm.T = T
         spins_dir = "$jobname.data/$(current_task_name(tm))"
         tm.outdir = spins_dir
         task(tm)
     end
 end
 
-job = JobInfo("$jobname", WignerMC{:Metropolis, bias_type};
+job = JobInfo("$jobname", WignerMC{:Metropolis, Nothing};
     run_time = "24:00:00",
     checkpoint_time = "30:00",
     tasks = make_tasks(tm),
