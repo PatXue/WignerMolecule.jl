8f32ce46cebb41b582520d312ac43ed33678eeef
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-09 11:28:18 -0400

    Sweep fm-high-t in terms of beta

--- jobs/fm-high-t.jl
+++ jobs/fm-high-t.jl
@@ -10,21 +10,22 @@ using WignerMolecule
 tm = TaskMaker()
 jobname = "fm-high-t"
 
-tm.sweeps = 50000
-tm.thermalization = 50000
-tm.binsize = 500
+tm.sweeps = 20000
+tm.thermalization = 20000
+tm.binsize = 200
 tm.init_type = :const
 tm.bias = nothing
 
 raw_params = load_object("all_params.jld2")[(45, 5, 20, 9)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-Ts = Iterators.flatten((0.05:0.025:0.1, 0.2:0.1:2.0))
+βs = Iterators.flatten((0.25:0.25:0.75, 1:2:20))
 Ls = [8]
 for L in Ls
     tm.Lx = tm.Ly = L
-    for T in Ts
-        tm.T = T
+    for β in βs
+        tm.β = β
+        tm.T = 1/β
         spins_dir = "$jobname.data/$(current_task_name(tm))"
         tm.outdir = spins_dir
         task(tm)
