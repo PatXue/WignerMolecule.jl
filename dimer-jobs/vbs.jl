44285437d844971e307f3097c9ce51ec7ea5684d
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-25 14:08:56 -0400

    Update dimer jobs to use new WignerParam constructor

--- dimer-jobs/vbs.jl
+++ dimer-jobs/vbs.jl
@@ -4,15 +4,12 @@ Pkg.activate("..")
 using Carlo
 using Carlo.JobTools
 using JLD2
-using LinearAlgebra
 using WignerMolecule
 
 tm = TaskMaker()
 jobname = "vbs"
 
-raw_params = load_object("all_params.jld2")[(45, 11, 20, 6)]
-norm_params = raw_params ./ norm(raw_params)
-tm.wigparams = WignerParams(norm_params...)
+tm.wigparams = WignerParams("all_params.jld2", 10, 6)
 
 Ts = 0.01:0.01:0.1
 Ls = [24]
