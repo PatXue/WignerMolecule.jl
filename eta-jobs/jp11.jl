5344a46700be525f58ceca0d1de5874ae9ebc5ab
Patrick Xue <xuepat360@gmail.com>
2026-07-07 14:50:02 -0400

    Rerun jp11 tracking all eta correlations

--- eta-jobs/jp11.jl
+++ eta-jobs/jp11.jl
@@ -9,10 +9,11 @@ tm = TaskMaker()
 jobname = "jp11"
 tm.init_type = :stripe
 tm.B = 0.01
+tm.allchis = true
 
-Ls = [24, 48]
+Ls = [24]
 Jzs = [0.5, 0.95, 1.0, 1.05, 1.125, 1.2]
-Ts = 0.3:0.05:1.0
+Ts = 0.3:0.1:1.7
 for (T, Jz, L) in Iterators.product(Ts, Jzs, Ls)
     tm.sweeps = 40000
     tm.thermalization = 40000
