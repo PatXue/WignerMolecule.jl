016c65d6f324f073e6052c04fdcbf44b171f3f7c
Patrick Xue <xuepat360@gmail.com>
2026-07-08 12:55:37 -0400

    Change xxz primary sweep to T

--- eta-jobs/xxz.jl
+++ eta-jobs/xxz.jl
@@ -11,13 +11,13 @@ tm.init_type = :rand
 tm.init_T = 3.0
 tm.B = 0.01
 
-Ls = [24,48]
-Ts = [0.25, 0.75, 1.1, 1.5, 2.0]
-Jzs = 0.75:0.05:1.25
-for (Jz, T, L) in Iterators.product(Jzs, Ts, Ls)
-    tm.sweeps = 20000
-    tm.thermalization = 20000
-    tm.binsize = 200
+Ls = [24, 48]
+Jzs = [0.75, 0.9, 0.95, 1.05, 1.2, 1.5]
+Ts = 0.2:0.2:2.0
+for (T, Jz, L) in Iterators.product(Ts, Jzs, Ls)
+    tm.sweeps = 40000
+    tm.thermalization = 40000
+    tm.binsize = 400
     tm.wigparams = EtaParams(Jz, 0.0)
     tm.T = T
     tm.Lx = tm.Ly = L
