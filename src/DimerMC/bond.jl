cba03f3ff2dbd1322e4b6abd3e4427fa76dd2ab2
PatXue <95881915+PatXue@users.noreply.github.com>
2026-07-13 15:23:50 -0400

    Also remove outdated collisions function

--- src/DimerMC/bond.jl
+++ src/DimerMC/bond.jl
@@ -12,21 +12,6 @@ function Base.mod1(d::Dimer, mc::DimerMC)
 end
 mod_equiv(pos, posj, mc::DimerMC) = all((pos .- posj) .% size(mc.spins) .== (0,0))
 
-# Return dimers that d conflicts with in mc
-function collisions(mc::DimerMC, d::Dimer)
-    res = []
-    if mod_equiv(mc.spins[d.pos...], d.posj, mc)
-        return res
-    end
-    if !mc.visited[d.pos...]
-        push!(res, Dimer(d.pos, mc.spins[d.pos...]))
-    end
-    if !mc.visited[d.posj...]
-        push!(res, Dimer(d.posj, mc.spins[d.posj...]))
-    end
-    return res
-end
-
 const disps = (SVector(1,0), SVector(-1,1), SVector(0,-1), SVector(-1,0), SVector(1,-1), SVector(0,1))
 const oriented_disps = (SVector(1,0), SVector(-1,1), SVector(0,-1))
 # Check and flip dimer to lie along a1, a2, or a3
@@ -51,3 +36,5 @@ function getν(d::Dimer, mc::DimerMC)
         throw(ArgumentError("Dimer $d not oriented correctly"))
     end
 end
+
+ismonomer(pos, mc::DimerMC) = mod_equiv(mc.spins[pos...], pos, mc)
