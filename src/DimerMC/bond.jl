8507e454e5a1d31896bf9b205372e599fff71040
PatXue <95881915+PatXue@users.noreply.github.com>
2026-07-13 15:21:50 -0400

    Remove unused helper funcs in bond.jl

--- src/DimerMC/bond.jl
+++ src/DimerMC/bond.jl
@@ -1,5 +1,4 @@
-# Helper functions for handling Bonds
-
+# Helper functions for handling indexing and dimers
 struct Dimer
     pos::SVector{2, Int}
     posj::SVector{2, Int}
@@ -12,24 +11,6 @@ function Base.mod1(d::Dimer, mc::DimerMC)
     return Dimer(mod1.(d.pos, dims), mod1.(d.posj, dims))
 end
 mod_equiv(pos, posj, mc::DimerMC) = all((pos .- posj) .% size(mc.spins) .== (0,0))
-
-# Shift position by v
-shift(d::Dimer, v) = Dimer(d.pos .+ v, d.posj .+ v)
-
-# Reflect position across x-axis
-const reflect1_mat = SMatrix{2,2}(1, 0, 1, -1)
-reflect1(d::Dimer) = Dimer(reflect1_mat * d.pos, reflect1_mat * d.posj)
-# Reflect position across line 30 deg above x-axis
-const reflect2_mat = SMatrix{2,2}(0, 1, 1, 0)
-reflect2(d::Dimer) = Dimer(reflect2_mat * d.pos, reflect2_mat * d.posj)
-
-# Rotation matrices in increments of 60 degrees
-const rotmats = Tuple(SMatrix{2,2}(0, 1, -1, 1)^i for i in 0:5)
-
-# Rotate around (0,0) by r*60 degrees
-rotate(v, r) = rotmats[r+1] * v
-rotate(d::Dimer, r) = Dimer(rotate(d.pos, r), rotate(d.posj, r))
-invrotate(x, r) = rotate(x, mod(6-r, 6))
 
 # Return dimers that d conflicts with in mc
 function collisions(mc::DimerMC, d::Dimer)
@@ -47,8 +28,8 @@ function collisions(mc::DimerMC, d::Dimer)
 end
 
 const disps = (SVector(1,0), SVector(-1,1), SVector(0,-1), SVector(-1,0), SVector(1,-1), SVector(0,1))
-# Check and flip dimer to lie along a1, a2, or a3
 const oriented_disps = (SVector(1,0), SVector(-1,1), SVector(0,-1))
+# Check and flip dimer to lie along a1, a2, or a3
 function orientdimer(d::Dimer, mc::DimerMC)
     disp = mod1(d.posj - d.pos, mc)
     if any([mod_equiv(disp, a, mc) for a in oriented_disps])
