8a2ed47c46f952e438507d47786978f529409534
PatXue <95881915+PatXue@users.noreply.github.com>
2026-07-16 18:58:50 -0400

    Calculate old and new energies in each case of sweep_s

--- src/DimerMC/sweep.jl
+++ src/DimerMC/sweep.jl
@@ -29,10 +29,24 @@ function sweep_s!(mc::DimerMC, ctx::Carlo.MCContext)
     for _ in 1:length(mc.spins)
         pos = SVector(rand(rng, 1:Lx), rand(rng, 1:Ly))
         posj = pos + rand(rng, disps)
-        d = Dimer(pos, posj)
         if mod_equiv(mc.spins[pos...], posj, mc)         # Dimer dissolution
+            s = rand(SpinVector)
+            sj = rand(SpinVector)
+            old_E = dimer_energy_s(mc, Dimer(pos, posj))
+            new_E = pair_energy_s(mc, pos, posj, s, sj)
         elseif ismonomer(pos, mc) && ismonomer(posj, mc) # Dimer creation
+            s = mc.spins[pos...]
+            sj = mc.spins[posj...]
+            old_E = pair_energy_s(mc, pos, posj, s, sj)
+            new_E = dimer_energy_s(mc, Dimer(pos, posj))
         elseif ismonomer(pos, mc) || ismonomer(posj, mc) # Monomer shift
+            if ismonomer(posj, mc)
+                pos, posj = posj, pos
+            end
+            s = rand(SpinVector)
+            posk = mc.spins[posj...]
+            old_E = shift_energy_s(mc, Dimer(posj, posk), pos, mc.spins[pos...])
+            new_E = shift_energy_s(mc, Dimer(pos, posj), posk, s)
         else                                             # Adjacent dimers
             continue
         end
