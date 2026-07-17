6614df1cc561f20b4f76a31b8f985d31ad0343f2
PatXue <95881915+PatXue@users.noreply.github.com>
2026-07-16 23:09:51 -0400

    Mix dimer and monomer updates (monomer update incomplete)

--- src/DimerMC/sweep.jl
+++ src/DimerMC/sweep.jl
@@ -21,57 +21,63 @@ function sweep_η!(mc::DimerMC, ctx::Carlo.MCContext)
     end
 end
 
-function sweep_s!(mc::DimerMC, ctx::Carlo.MCContext)
+function sweep_dimer!(mc::DimerMC, ctx::Carlo.MCContext)
     Lx, Ly = size(mc.spins)
-    T = calc_temp(mc, ctx)
     rng = ctx.rng
+    pos = SVector(rand(rng, 1:Lx), rand(rng, 1:Ly))
+    posj = pos + rand(rng, disps)
 
-    for _ in 1:length(mc.spins)
-        pos = SVector(rand(rng, 1:Lx), rand(rng, 1:Ly))
-        posj = pos + rand(rng, disps)
-        if mod_equiv(mc.spins[pos...], posj, mc)         # Dimer dissolution
-            s = rand(SpinVector)
-            sj = rand(SpinVector)
-            old_E = dimer_energy_s(mc, Dimer(pos, posj))
-            new_E = pair_energy_s(mc, pos, posj, s, sj)
-            if metropolisacc(mc, ctx, new_E - old_E)
-                mc.spins[pos...] = pos
-                mc.spins[posj...] = posj
-                mc.monospins[pos...] = s
-                mc.monospins[posj...] = sj
-            end
-        elseif ismonomer(pos, mc) && ismonomer(posj, mc) # Dimer creation
-            s = mc.monospins[pos...]
-            sj = mc.monospins[posj...]
-            old_E = pair_energy_s(mc, pos, posj, s, sj)
-            new_E = dimer_energy_s(mc, Dimer(pos, posj))
-            if metropolisacc(mc, ctx, new_E - old_E)
-                mc.spins[pos...] = posj
-                mc.spins[posj...] = pos
-            end
-        elseif ismonomer(pos, mc) || ismonomer(posj, mc) # Monomer shift
-            if ismonomer(posj, mc)
-                pos, posj = posj, pos
-            end
-            s = mc.monospins[pos...]
-            sk = rand(SpinVector)
-            posk = mc.spins[posj...]
-            old_E = shift_energy_s(mc, Dimer(posj, posk), pos, s)
-            new_E = shift_energy_s(mc, Dimer(pos, posj), posk, sk)
-            if metropolisacc(mc, ctx, new_E - old_E)
-                mc.spins[pos...] = posj
-                mc.spins[posj...] = pos
-                mc.spins[posk...] = posk
-                mc.monospins[posk...] = sk
-            end
-        else                                             # Adjacent dimers
-            continue
+    if mod_equiv(mc.spins[pos...], posj, mc) # Dimer dissolution
+        s = rand(rng, SpinVector)
+        sj = rand(rng, SpinVector)
+        old_E = dimer_energy_s(mc, Dimer(pos, posj))
+        new_E = pair_energy_s(mc, pos, posj, s, sj)
+        if metropolisacc(mc, ctx, new_E - old_E)
+            mc.spins[pos...] = pos
+            mc.spins[posj...] = posj
+            mc.monospins[pos...] = s
+            mc.monospins[posj...] = sj
+        end
+    elseif ismonomer(pos, mc) && ismonomer(posj, mc) # Dimer creation
+        s = mc.monospins[pos...]
+        sj = mc.monospins[posj...]
+        old_E = pair_energy_s(mc, pos, posj, s, sj)
+        new_E = dimer_energy_s(mc, Dimer(pos, posj))
+        if metropolisacc(mc, ctx, new_E - old_E)
+            mc.spins[pos...] = posj
+            mc.spins[posj...] = pos
+        end
+    elseif ismonomer(pos, mc) || ismonomer(posj, mc) # Monomer shift
+        if ismonomer(posj, mc)
+            pos, posj = posj, pos
+        end
+        s = mc.monospins[pos...]
+        sk = rand(rng, SpinVector)
+        posk = mc.spins[posj...]
+        old_E = shift_energy_s(mc, Dimer(posj, posk), pos, s)
+        new_E = shift_energy_s(mc, Dimer(pos, posj), posk, sk)
+        if metropolisacc(mc, ctx, new_E - old_E)
+            mc.spins[pos...] = posj
+            mc.spins[posj...] = pos
+            mc.spins[posk...] = posk
+            mc.monospins[posk...] = sk
         end
     end
+end
 
-    if is_thermalized(ctx)
-        measure!(ctx, :dimersteps, steps)
-        measure!(ctx, :dimerretries, retries)
+function sweep_monomer!(mc::DimerMC, ctx::Carlo.MCContext)
+    Lx, Ly = size(mc.spins)
+    rng = ctx.rng
+    pos = SVector(rand(rng, 1:Lx), rand(rng, 1:Ly))
+end
+
+function sweep_s!(mc::DimerMC, ctx::Carlo.MCContext)
+    for _ in 1:length(mc.spins)
+        if rand(ctx.rng) < mc.Q
+            sweep_dimer!(mc, ctx)
+        else
+            sweep_monomer!(mc, ctx)
+        end
     end
 end
 
