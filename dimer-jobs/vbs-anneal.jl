bde8e6737770ff662b5dbc52e38b611abe439a85
PatXue <95881915+PatXue@users.noreply.github.com>
2026-07-17 15:59:23 -0400

    Create annealed (rand init) vbs job

---
+++ dimer-jobs/vbs-anneal.jl
@@ -0,0 +1,32 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "vbs-anneal"
+tm.Q = 0.5
+tm.init_T = 1.0
+tm.init_type = :rand
+tm.wigparams = WignerParams("all_params.jld2", 10, 6)
+
+Ts = 0.02:0.02:0.2
+Ls = [24]
+for (T, L) in Iterators.product(Ts, Ls)
+    tm.sweeps = 25000 * div(L, 24)
+    tm.thermalization = tm.sweeps
+    tm.binsize = div(tm.sweeps, 100)
+    tm.Lx = tm.Ly = L
+    tm.T = T
+    task(tm)
+end
+
+job = JobInfo("$jobname", DimerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
