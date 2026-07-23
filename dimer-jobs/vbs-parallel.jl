a4a62bbb2a6136190977eae1fbb3d9fa387e3cb5
Patrick Xue <xuepat360@gmail.com>
2026-07-23 10:41:51 -0400

    Run and plot DimerMC under parallel tempering

---
+++ dimer-jobs/vbs-parallel.jl
@@ -0,0 +1,36 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "vbs-parallel"
+tm.Q = 0.5
+tm.wigparams = WignerParams("all_params.jld2", 10, 6)
+
+tm.sweeps = 50000
+tm.binsize = 250
+Ts = 0.05:0.01:0.15
+Ls = [24]
+tm.parallel_tempering = (
+    mc = DimerMC,
+    parameter = :T,
+    values = Ts,
+    interval = 20
+)
+for L in Ls
+    tm.thermalization = 100000
+    tm.Lx = tm.Ly = L
+    task(tm)
+end
+
+job = JobInfo("$jobname", ParallelTemperingMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+    ranks_per_run = length(Ts)
+)
+start(job, ARGS)
