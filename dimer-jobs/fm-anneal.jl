cf7b423e7cd3fa85974385c6a19775ffb14d7f37
Patrick Xue <xuepat360@gmail.com>
2026-07-29 12:46:08 -0400

    Create job for annealed FM in dimer model

---
+++ dimer-jobs/fm-anneal.jl
@@ -0,0 +1,31 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "fm-anneal"
+tm.init_T = 4.0
+tm.init_type = :rand
+tm.wigparams = WignerParams("all_params.jld2", 5, 9)
+
+tm.sweeps = 50000
+tm.binsize = 250
+Ts = 0.01:0.01:0.2
+Ls = [24, 48]
+for (T, L) in Iterators.product(Ts, Ls)
+    tm.thermalization = 100000
+    tm.Lx = tm.Ly = L
+    tm.T = T
+    task(tm)
+end
+
+job = JobInfo("$jobname", DimerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
