52f0c44a10c83897a345140896a08eb003b8a35e
Patrick Xue <xuepat360@gmail.com>
2026-07-30 12:54:00 -0400

    Create job for infinite temp dimer run

---
+++ dimer-jobs/vbs-inf.jl
@@ -0,0 +1,30 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "vbs-inf"
+tm.wigparams = WignerParams("all_params.jld2", 10, 6)
+
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 250
+tm.T = Inf
+fugs = 0.1:0.1:1.0
+Ls = [24, 48]
+for (fug, L) in Iterators.product(fugs, Ls)
+    tm.Lx = tm.Ly = L
+    tm.fug = fug
+    task(tm)
+end
+
+job = JobInfo("$jobname", DimerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
