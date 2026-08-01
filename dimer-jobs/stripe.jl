4789f710f26453e35815974bfe01bc9f9cf5e50c
Patrick Xue <xuepat360@gmail.com>
2026-07-31 22:14:51 -0400

    Create stripe job in dimer model

---
+++ dimer-jobs/stripe.jl
@@ -0,0 +1,30 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "stripe"
+tm.init_type = :stripe
+tm.wigparams = WignerParams("all_params.jld2", 5, 6)
+
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 250
+Ts = 0.01:0.01:0.2
+Ls = [24, 48]
+for (T, L) in Iterators.product(Ts, Ls)
+    tm.Lx = tm.Ly = L
+    tm.T = T
+    task(tm)
+end
+
+job = JobInfo("$jobname", DimerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
