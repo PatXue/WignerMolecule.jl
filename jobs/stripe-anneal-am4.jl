f12d035c353a3313160308b7f3a2b493e0db2cfa
Patrick Xue <xuepat360@gmail.com>
2026-08-04 13:28:03 -0400

    Use same normalization (meV) for am sweep

--- jobs/stripe-anneal-am4.jl
+++ jobs/stripe-anneal-am4.jl
@@ -12,7 +12,7 @@ jobname = "stripe-anneal-am4"
 
 tm.init_type = :stripe
 tm.sweeps = 100000
-tm.thermalization = 50000
+tm.thermalization = 100000
 tm.binsize = 500
 
 stripe_bias(x, _) = [0, 0, (-1)^(div(x, 2))]
@@ -24,11 +24,11 @@ JSON.lower(f::bias_type) = f(1, 1)
 
 Ls = [48]
 ers = 5:11
-Ts = 0.01:0.01:0.15
+Ts = 0.3:0.3:4.5
 for (T, er, L) in Iterators.product(Ts, ers, Ls)
     tm.Lx = tm.Ly = L
     tm.er = er
-    tm.wigparams = WignerParams("all_params.jld2", er, 6)
+    tm.wigparams = WignerParams("all_params.jld2", er, 6, H0=1)
     tm.T = T
     task(tm)
 end
