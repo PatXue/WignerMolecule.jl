d8c9fb97ad33fa3cbd52d1122b62ad4839ad50d9
Patrick Xue <xuepat360@gmail.com>
2026-08-04 21:37:41 -0400

    Change init type between stripe/afm-afe

--- jobs/stripe-anneal-am4.jl
+++ jobs/stripe-anneal-am4.jl
@@ -8,24 +8,18 @@ using JSON
 using WignerMolecule
 
 tm = TaskMaker()
-jobname = "stripe-anneal-am4"
-
-tm.init_type = :stripe
+jobname = "am4"
+tm.bias = nothing
 tm.sweeps = 100000
 tm.thermalization = 100000
 tm.binsize = 500
 
-stripe_bias(x, _) = [0, 0, (-1)^(div(x, 2))]
-tm.bias = stripe_bias
-bias_type = typeof(stripe_bias)
-tm.B = 0.0
-tm.init_B = 5.0
-JSON.lower(f::bias_type) = f(1, 1)
-
+inits = [:stripe, :afm_afe]
 Ls = [48]
 ers = 5:11
 Ts = 0.3:0.3:4.5
-for (T, er, L) in Iterators.product(Ts, ers, Ls)
+for (T, er, L, init) in Iterators.product(Ts, ers, Ls, inits)
+    tm.init_type = init
     tm.Lx = tm.Ly = L
     tm.er = er
     tm.wigparams = WignerParams("all_params.jld2", er, 6, H0=1)
@@ -33,7 +27,7 @@ for (T, er, L) in Iterators.product(Ts, ers, Ls)
     task(tm)
 end
 
-job = JobInfo("$jobname", WignerMC{:Metropolis, bias_type};
+job = JobInfo("$jobname", WignerMC{:Metropolis, Nothing};
     run_time = "24:00:00",
     checkpoint_time = "30:00",
     tasks = make_tasks(tm),
