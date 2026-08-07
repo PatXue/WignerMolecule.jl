0d1bf363159db573d30655ec104e72bc74fbf656
Patrick Xue <xuepat360@gmail.com>
2026-08-06 21:03:45 -0400

    Create function to heatbath sample spins/etas

--- src/sweep.jl
+++ src/sweep.jl
@@ -3,6 +3,37 @@ function metropolisacc(ΔE, T; rng, fug=1.0)
     return prob >= 1.0 || rand(rng) <= prob
 end
 
+"""
+    heatbath_spin(Bvec, T; rng)
+
+Given the local field Bvec on a site, sample a spin from the local Boltzmann distribution
+"""
+function heatbath_spin(Bvec, T; rng)
+    B = norm(Bvec)
+    if isapprox(B, 0.0, atol=1e-5)
+        return rand(rng, SpinVector)
+    end
+
+    # Generate parallel component
+    Bhat = Bvec / B
+    c = B / 2T
+    cosθ = -log(exp(c) - 2*rand(rng)*sinh(c)) / c
+    s = Bhat * cosθ
+    # Find a basis of B's orthogonal subspace
+    u = Bhat × SVector(1,0,0)
+    if isapprox(norm(u), 0.0, atol=1e-4)
+        u = Bhat × SVector(0,1,0)
+    end
+    u /= norm(u)
+    v = Bhat × u
+    # Generate orthogonal component
+    ϕ = 2pi * rand(rng)
+    s += sqrt(1-norm(s)^2) * (u*cos(ϕ) + v*sin(ϕ))
+
+    @assert norm(s) ≈ 1.0
+    return s
+end
+
 function flip_eta!(mc::WignerMC{:Metropolis}, T, rng)
     Lx, Ly = size(mc.ηs)
     pos = SVector(rand(rng, 1:Lx), rand(rng, 1:Ly))
