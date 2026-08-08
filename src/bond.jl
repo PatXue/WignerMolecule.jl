2314dfe3d2b26384fb2914dcfc646030a57463fa
Patrick Xue <xuepat360@gmail.com>
2026-08-07 21:01:43 -0400

    isoriented function

--- src/bond.jl
+++ src/bond.jl
@@ -14,15 +14,17 @@ mod_equiv(pos, posj, mc) = all((pos .- posj) .% size(mc.spins) .== (0,0))
 
 const disps = (SVector(1,0), SVector(-1,1), SVector(0,-1), SVector(-1,0), SVector(1,-1), SVector(0,1))
 const oriented_disps = (SVector(1,0), SVector(-1,1), SVector(0,-1))
+
+isoriented(d::Dimer, mc) = any([mod_equiv(d.posj - d.pos, a, mc) for a in oriented_disps])
 # Check and flip dimer to lie along a1, a2, or a3
 function orientdimer(d::Dimer, mc)
-    disp = d.posj - d.pos
-    if any([mod_equiv(disp, a, mc) for a in oriented_disps])
+    if isoriented(d, mc)
         return d
     else
         return Dimer(d.posj, d.pos)
     end
 end
+
 # Get the ν coupling factor for a dimer (assuming dimer oriented)
 function getν(d::Dimer, mc)
     disp = d.posj - d.pos
