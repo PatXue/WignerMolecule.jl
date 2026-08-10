9697ffde0ee760492dbd7f1f90d84ad958221307
Patrick Xue <xuepat360@gmail.com>
2026-08-10 13:43:08 -0400

    Create job for am8 low er sweep

---
+++ jobs/am8-low.jl
@@ -0,0 +1,35 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using JSON
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "am8-low"
+tm.bias = nothing
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 500
+
+inits = [:stripe, :afm_afe, :afm_fe]
+Ls = [48]
+ers = 5:7
+Ts = 0.2:0.2:3.0
+for (T, er, L, init) in Iterators.product(Ts, ers, Ls, inits)
+    tm.init_type = init
+    tm.Lx = tm.Ly = L
+    tm.er = er
+    tm.wigparams = WignerParams("all_params.jld2", er, 8, H0=1)
+    tm.T = T
+    task(tm)
+end
+
+job = JobInfo("$jobname", WignerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
