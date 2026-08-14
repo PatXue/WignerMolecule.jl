32ef2151efea5ead07fc42da514bdf214fa3f8a8
Patrick Xue <xuepat360@gmail.com>
2026-08-14 13:27:27 -0400

    Increase afm-fe-vbs temp and fug range

--- dimer-jobs/afm-fe-vbs.jl
+++ dimer-jobs/afm-fe-vbs.jl
@@ -9,15 +9,15 @@ using WignerMolecule
 tm = TaskMaker()
 jobname = "afm-fe-vbs"
 tm.init_type = :vbs
-tm.wigparams = WignerParams("all_params.jld2", 10, 10)
+tm.wigparams = WignerParams("all_params.jld2", 11, 10)
 
-tm.sweeps = 50000
+tm.sweeps = 100000
 tm.thermalization = 100000
 tm.binsize = 500
-Ts = 0.02:0.02:0.2
 Ls = [48]
-fugs = [0.01, 0.05, 0.15, 0.25, 0.5, 0.75]
-for (T, L, fug) in Iterators.product(Ts, Ls, fugs)
+fugs = [0.1, 0.2, 0.5, 1.0, 5.0, 10.0]
+Ts = 0.05:0.05:0.5
+for (T, fug, L) in Iterators.product(Ts, fugs, Ls)
     tm.Lx = tm.Ly = L
     tm.T = T
     tm.fug = fug
