c4759677168e61507694c67292d401293fbdf721
Patrick Xue <xuepat360@gmail.com>
2026-08-13 21:32:41 -0400

    Rerun fm-anneal with heatbath and track Binder ratio

--- jobs/fm.jl
+++ jobs/fm.jl
@@ -11,25 +11,25 @@ using WignerMolecule
 tm = TaskMaker()
 jobname = "fm-anneal"
 
-tm.sweeps = 50000
-tm.binsize = 250
+tm.algtype = :Heatbath
+tm.sweeps = 100000
+tm.thermalization = 100000
+tm.binsize = 500
 tm.init_type = :const
 
 fm_bias(_, _) = [0, 0, 1]
 tm.bias = fm_bias
-bias_type = typeof(fm_bias)
 tm.init_B = 5.0
 tm.B = 0
-JSON.lower(f::bias_type) = f(1, 1)
+JSON.lower(f::(typeof(fm_bias))) = f(1, 1)
 
 raw_params = load_object("all_params.jld2")[(45, 5, 20, 9)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
-Ts = 0.01:0.01:0.2
+Ts = 0.01:0.01:0.15
 Ls = [24, 48, 96, 120]
 for L in Ls
     tm.Lx = tm.Ly = L
-    tm.thermalization = 50000 * div(L, 24)
     for T in Ts
         tm.T = T
         spins_dir = "$jobname.data/$(current_task_name(tm))"
@@ -38,7 +38,7 @@ for L in Ls
     end
 end
 
-job = JobInfo("$jobname", WignerMC{:Metropolis, bias_type};
+job = JobInfo("$jobname", WignerMC;
     run_time = "24:00:00",
     checkpoint_time = "30:00",
     tasks = make_tasks(tm),
