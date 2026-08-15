b20919ea30a3c2370f30ea3a2f2d8c7ed4a75a49
Patrick Xue <xuepat360@gmail.com>
2026-08-15 16:39:33 -0400

    Spin only afm-fe param dimer run

---
+++ dimer-jobs/afm-fe-spin.jl
@@ -0,0 +1,33 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "afm-fe-spin"
+tm.init_type = :vbs_eta
+tm.algtype = :SpinOnly
+tm.wigparams = WignerParams("all_params.jld2", 11, 10)
+
+tm.sweeps = 100000
+tm.thermalization = 100000
+tm.binsize = 1000
+Ls = [48]
+fugs = [0.2, 0.5, 1.0, 3.0]
+Ts = 0.02:0.02:0.2
+for (T, fug, L) in Iterators.product(Ts, fugs, Ls)
+    tm.Lx = tm.Ly = L
+    tm.T = T
+    tm.fug = fug
+    task(tm)
+end
+
+job = JobInfo("$jobname", DimerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
