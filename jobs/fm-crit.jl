58483f63fedb9db9a63e55270d54d06ebae83333
Patrick Xue <xuepat360@gmail.com>
2026-08-16 12:31:09 -0400

    Create job for FM at critical temperature

---
+++ jobs/fm-crit.jl
@@ -0,0 +1,36 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using JSON
+using LinearAlgebra
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "fm-anneal"
+
+tm.algtype = :Heatbath
+tm.sweeps = 100000
+tm.thermalization = 100000
+tm.binsize = 1000
+tm.init_type = :const
+
+tm.wigparams = WignerParams("all_params.jld2", 5, 9)
+Ts = 0.0525:0.0025:0.1
+Ls = [24, 48, 96]
+for L in Ls
+    tm.Lx = tm.Ly = L
+    for T in Ts
+        tm.T = T
+        task(tm)
+    end
+end
+
+job = JobInfo("$jobname", WignerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
