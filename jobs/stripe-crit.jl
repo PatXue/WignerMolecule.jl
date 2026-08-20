cdd697d7a737a99673d519b4ba7757b140be9a0f
Patrick Xue <xuepat360@gmail.com>
2026-08-20 11:05:15 -0400

    Create job for sweeping stripe critical T

---
+++ jobs/stripe-crit.jl
@@ -0,0 +1,35 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using JSON
+using LinearAlgebra
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "stripe-crit"
+tm.init_type = :stripe
+tm.algtype = :Heatbath
+
+tm.wigparams = WignerParams("all_params.jld2", 5, 6)
+tm.sweeps = 200000
+tm.thermalization = 100000
+tm.binsize = 1000
+Ls = [24, 48, 72]
+Ts = 0.07:0.0025:0.1
+for L in Ls
+    tm.Lx = tm.Ly = L
+    for T in Ts
+        tm.T = T
+        task(tm)
+    end
+end
+
+job = JobInfo("$jobname", WignerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
