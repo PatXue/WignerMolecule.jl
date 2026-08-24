a676fff5530762cee050e8b9971cfd1ff0818fd2
Patrick Xue <xuepat360@gmail.com>
2026-08-23 23:47:43 -0400

    Coarser stripe-crit T sweep

--- jobs/stripe-crit.jl
+++ jobs/stripe-crit.jl
@@ -17,8 +17,8 @@ tm.wigparams = WignerParams("all_params.jld2", 5, 6)
 tm.sweeps = 200000
 tm.thermalization = 200000
 tm.binsize = 1000
-Ls = [24, 48, 96]
-Ts = 0.08:0.0001:0.082
+Ls = [48, 72, 96]
+Ts = 0.08:0.0002:0.082
 for L in Ls
     tm.Lx = tm.Ly = L
     for T in Ts
