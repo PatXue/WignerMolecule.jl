fde583be1a68933db4a523fbe89c725a2a0d3e33
Patrick Xue <xuepat360@gmail.com>
2026-08-26 21:01:55 -0400

    Run afm-fe-crit with cluster spin updates

--- jobs/afm-fe-crit.jl
+++ jobs/afm-fe-crit.jl
@@ -10,22 +10,20 @@ using WignerMolecule
 tm = TaskMaker()
 jobname = "afm-fe-crit"
 tm.init_type = :afm_fe
-tm.algtype = :Heatbath
-
 tm.sweeps = 100000
 tm.thermalization = 100000
 tm.binsize = 1000
 
 tm.wigparams = WignerParams("all_params.jld2", 11, 10)
-Ts = 0.008:0.008:0.08
-Ls = [24, 48, 72]
+Ts = Iterators.flatten((0.05, 0.0505:0.0001:0.0525))
+Ls = [48]
 for (T, L) in Iterators.product(Ts, Ls)
     tm.Lx = tm.Ly = L
     tm.T = T
     task(tm)
 end
 
-job = JobInfo("$jobname", WignerMC;
+job = JobInfo("$jobname", ClusterWigMC;
     run_time = "24:00:00",
     checkpoint_time = "30:00",
     tasks = make_tasks(tm),
