03693b33e5ada52d5cd73e0e7321534afde40435
Patrick Xue <xuepat360@gmail.com>
2026-08-28 10:00:54 -0400

    Run afm-fe-crit with longer sweeps on critical T

--- jobs/afm-fe-crit.jl
+++ jobs/afm-fe-crit.jl
@@ -10,16 +10,17 @@ using WignerMolecule
 tm = TaskMaker()
 jobname = "afm-fe-crit"
 tm.init_type = :afm_fe
-tm.sweeps = 100000
+tm.sweeps = 200000
 tm.thermalization = 100000
 tm.binsize = 1000
 
 tm.wigparams = WignerParams("all_params.jld2", 11, 10)
-Ts = Iterators.flatten((0.05, 0.0505:0.0001:0.0525))
-Ls = [48]
+Ts = Iterators.flatten((0.0505, 0.051:0.0001:0.0524))
+Ls = [48, 72]
 for (T, L) in Iterators.product(Ts, Ls)
     tm.Lx = tm.Ly = L
     tm.T = T
+    tm.f = 0.0512 <= T <= 0.0518 ? 2 : 1
     task(tm)
 end
 
