8335f176d7fe8fc47024cb3f0f9bb1995e72abb0
Patrick Xue <xuepat360@gmail.com>
2026-08-28 16:31:53 -0400

    Rerun afm-fe-anneal to check autocorr times

--- jobs/afm-fe-anneal.jl
+++ jobs/afm-fe-anneal.jl
@@ -11,29 +11,23 @@ using WignerMolecule
 tm = TaskMaker()
 jobname = "afm-fe-anneal"
 tm.init_type = :afm_fe
-
-afm_bias(x, _) = [0, 0, (-1)^x]
-tm.bias = afm_bias
-bias_type = typeof(afm_bias)
-tm.B = 0.0
-tm.init_B = 5.0
-JSON.lower(f::bias_type) = f(1, 1)
+tm.algtype = :Heatbath
 
 raw_params = load_object("all_params.jld2")[(45, 11, 20, 10)]
 norm_params = raw_params ./ norm(raw_params)
 tm.wigparams = WignerParams(norm_params...)
 Ts = 0.004:0.004:0.08
-Ls = [24, 48, 96, 120]
+Ls = [48, 72, 96, 120]
 for (T, L) in Iterators.product(Ts, Ls)
     tm.Lx = tm.Ly = L
     tm.sweeps = 50000
-    tm.binsize = 250
-    tm.thermalization = 50000 * div(L, 24)
+    tm.binsize = 500
+    tm.thermalization = 50000
     tm.T = T
     task(tm)
 end
 
-job = JobInfo("$jobname", WignerMC{:Metropolis, bias_type};
+job = JobInfo("$jobname", WignerMC;
     run_time = "24:00:00",
     checkpoint_time = "30:00",
     tasks = make_tasks(tm),
