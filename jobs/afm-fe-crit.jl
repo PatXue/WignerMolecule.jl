61511bdebe64caa1d51ca5bb7748b358d5f1dac9
Patrick Xue <xuepat360@gmail.com>
2026-08-31 09:39:17 -0400

    Run afm-fe-crit with parallel tempering

--- jobs/afm-fe-crit.jl
+++ jobs/afm-fe-crit.jl
@@ -10,23 +10,30 @@ using WignerMolecule
 tm = TaskMaker()
 jobname = "afm-fe-crit"
 tm.init_type = :afm_fe
+tm.algtype = :Cluster
 tm.sweeps = 200000
-tm.thermalization = 300000
+tm.thermalization = 200000
 tm.binsize = 2000
 tm.f = 2
 
 tm.wigparams = WignerParams("all_params.jld2", 11, 10)
-Ts = Iterators.flatten((0.051:0.00005:0.0515, 0.052))
+Ts = collect(Iterators.flatten((0.051:0.00005:0.0515, 0.052)))
 Ls = [48, 72]
-for (T, L) in Iterators.product(Ts, Ls)
+tm.parallel_tempering = (
+    mc = WignerMC,
+    parameter = :T,
+    values = Ts,
+    interval = 10
+)
+for L in Ls
     tm.Lx = tm.Ly = L
-    tm.T = T
     task(tm)
 end
 
-job = JobInfo("$jobname", ClusterWigMC;
+job = JobInfo("$jobname", ParallelTemperingMC;
     run_time = "24:00:00",
     checkpoint_time = "30:00",
     tasks = make_tasks(tm),
+    ranks_per_run = length(Ts)
 )
 start(job, ARGS)
