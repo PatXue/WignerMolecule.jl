93767fb417f5602ac10ea83c1ef179c33e18cbee
Patrick Xue <xuepat360@gmail.com>
2026-09-04 23:15:30 -0400

    Run er7 with quarter spaced am sweep

--- jobs/er7.jl
+++ jobs/er7.jl
@@ -12,18 +12,18 @@ jobname = "er7"
 tm.algtype = :Heatbath
 tm.init_type = :rand
 tm.init_T = 4.0
-tm.sweeps = 150000
+tm.sweeps = 100000
 tm.thermalization = 100000
 tm.binsize = 1000
 
-Ls = [24, 48]
+Ls = [48]
 Ts = [0.5, 1.0, 1.5, 2.0, 3.0, 4.0]
-ams = 4:11
+ams = 4:0.25:11
 for (am, T, L) in Iterators.product(ams, Ts, Ls)
-    tm.corr_rad = div(L, 12)
+    tm.corr_rad = 2
     tm.Lx = tm.Ly = L
     tm.am = am
-    tm.wigparams = WignerParams("all_params.jld2", 7, am, H0=1)
+    tm.wigparams = WignerParams("params_interpolated_quarter.jld2", (am, 7), H0=1)
     tm.T = T
     task(tm)
 end
