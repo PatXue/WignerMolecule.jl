da2ed755c35b25a8f9b0268551dc418c91f0f892
Patrick Xue <xuepat360@gmail.com>
2026-09-05 23:33:29 -0400

    Run stripe with vbs init

---
+++ dimer-jobs/stripe-vbs.jl
@@ -0,0 +1,32 @@
+import Pkg
+Pkg.activate("..")
+
+using Carlo
+using Carlo.JobTools
+using JLD2
+using WignerMolecule
+
+tm = TaskMaker()
+jobname = "stripe"
+tm.init_type = :vbs
+tm.wigparams = WignerParams("all_params.jld2", 5, 6)
+
+tm.sweeps = 50000
+tm.thermalization = 50000
+tm.binsize = 500
+Ts = 0.02:0.02:0.2
+Ls = [48]
+fugs = [0.05, 0.25, 1.0, 4.0]
+for (T, L, fug) in Iterators.product(Ts, Ls, fugs)
+    tm.fug = fug
+    tm.Lx = tm.Ly = L
+    tm.T = T
+    task(tm)
+end
+
+job = JobInfo("$jobname", DimerMC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
