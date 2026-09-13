229297eef52c86a3521ec9b0c3e0b3b970c5ae14
Patrick Xue <xuepat360@gmail.com>
2026-09-13 01:31:37 -0400

    Narrow stripe=crit T range and increase therm

--- jobs/stripe-crit.jl
+++ jobs/stripe-crit.jl
@@ -13,18 +13,18 @@ jobname = "stripe-crit"
 tm.init_type = :stripe
 tm.algtype = :Cluster
 tm.sweeps = 200000
-tm.thermalization = 300000
+tm.thermalization = 200000
 tm.binsize = 2000
 
 tm.wigparams = WignerParams("all_params.jld2", 5, 6)
-Ls = [48, 72, 96]
-Ts = range(0.079, 0.081, 10)
+Ls = [48, 60, 72]
+Ts = range(0.08, 0.0812, 12)
 tm.Ts = Ts
 tm.parallel_tempering = (
     mc = WignerMC,
     parameter = :T,
     values = Ts,
-    interval = 5
+    interval = 1
 )
 for L in Ls
     tm.Lx = tm.Ly = L
